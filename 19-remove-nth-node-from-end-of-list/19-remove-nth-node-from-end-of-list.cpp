fb6c620d85d918a832441d1e520f2bc5e99f829a
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-04-15 01:51:16 +0900

    Time: 7 ms (49.24%), Space: 10.6 MB (75.16%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -2,23 +2,27 @@ class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode *currentNode = head;
-        //move currentNode n steps into list
+        ListNode *nodeBeforeRemoved = head;
+        
+        //for loop n times to make a (0 to nth)gap between currenNode and nodeBeforeRemoved
         for(int i = 0; i < n; i++){
             currentNode = currentNode->next;
         }
+        //if n is size of head which removes the first element in the linked list
         if(currentNode == NULL){
             return head->next;
         }
-        //move both pointers until currentNode reaches the end of list
-        ListNode *nodeBeforeRemoved = head;
-        
+        //move both pointers until currentNode reaches end of the linked list
         while(currentNode->next != NULL){
             currentNode = currentNode->next;
-            nodeBeforeRemoved = nodeBeforeRemoved->next;   
+            nodeBeforeRemoved = nodeBeforeRemoved->next;
         }
         
+        //now make nodeBeforeRemoved points to the pointer after nth node
         nodeBeforeRemoved->next = nodeBeforeRemoved->next->next;
         
+        //return the head
         return head;
+        
     }
 };
