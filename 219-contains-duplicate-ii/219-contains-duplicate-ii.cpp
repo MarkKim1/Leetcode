7d046ac9565ce230bf614a37b4de1840f9062d07
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-04-20 19:04:39 +0900

    Time: 178 ms (85.11%), Space: 77.2 MB (28.63%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -3,7 +3,7 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_map<int,int> map;
         for(int i =0; i < nums.size(); i++){
-            if(map.find(nums[i]) != map.end() &&abs(i - map[nums[i]] <= k)){
+            if(map.find(nums[i]) != map.end() &&i - map[nums[i]] <= k){
                     return true;
             }
             map[nums[i]] = i;
