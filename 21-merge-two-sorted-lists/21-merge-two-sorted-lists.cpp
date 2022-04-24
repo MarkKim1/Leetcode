53b9725613c889f390913f343f8a89bec958dbb5
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-04-24 15:19:35 +0900

    Time: 18 ms (13.29%), Space: 14.8 MB (82.08%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -22,17 +22,17 @@ public:
         }
         while(list1 != NULL && list2 != NULL){
             if(list1->val <= list2->val){
-                head->next = list1;
+                temp->next = list1;
                 list1 = list1->next;
             }
             else{
-                head->next = list2;
+                temp->next = list2;
                 list2 = list2->next;
             }
-            head = head->next;
+            temp = temp->next;
             
         }
-        list2 != NULL ? head->next = list2: head->next = list1; 
-        return temp->next;
+        list2 != NULL ? temp->next = list2: temp->next = list1; 
+        return head->next;
     }
 };
