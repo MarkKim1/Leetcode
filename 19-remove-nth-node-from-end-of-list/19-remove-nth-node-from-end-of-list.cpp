217473414755b7f3d7237ab9f0da0673fc5d9297
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-04-26 01:06:15 +0900

    Time: 7 ms (48.66%), Space: 10.7 MB (76.00%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -1,28 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode *currentNode = head;
-        ListNode *nodeBeforeRemoved = head;
-        
-        //for loop n times to make a (0 to nth)gap between currenNode and nodeBeforeRemoved
-        for(int i = 0; i < n; i++){
-            currentNode = currentNode->next;
+        ListNode *first = head;
+        ListNode *second = head;
+        for(int i = 0;i < n; i++){
+            first = first->next;
         }
-        //if n is size of head which removes the first element in the linked list
-        if(currentNode == NULL){
+        if(first == NULL){
             return head->next;
         }
-        //move both pointers until currentNode reaches end of the linked list
-        while(currentNode->next != NULL){
-            currentNode = currentNode->next;
-            nodeBeforeRemoved = nodeBeforeRemoved->next;
+        while(first->next != NULL){
+            first = first->next;
+            second = second->next;
         }
+        second->next = second->next->next;
         
-        //now make nodeBeforeRemoved points to the pointer after nth node
-        nodeBeforeRemoved->next = nodeBeforeRemoved->next->next;
-        
-        //return the head
         return head;
-        
     }
 };
