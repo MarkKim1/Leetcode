dca63599a6f2b0c5127494ec04fee6023db28483
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-05-06 22:54:46 +0900

    Time: 28 ms (36.87%), Space: 8.7 MB (73.18%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> ans;
+        vector<int> hash(26, 0);
+        vector<int> phash(26, 0);
+        int window = p.size();
+        int len = s.size();
+        if(len < window)
+        {
+            return ans;
+        }
+        int left = 0,right = 0;
+        while(right < window)
+        {
+            phash[p[right] - 'a'] ++;
+            hash[s[right] - 'a'] ++;
+            right++;
+        }
+        right--;
+        while(right < len)
+        {
+            if(phash == hash)
+            {
+                ans.push_back(left);
+            }
+            right++;
+            if(right != len)
+            {
+                hash[s[right] - 'a']++;
+            }
+            hash[s[left] - 'a']--;
+            left++;
+        }
+        return ans;
+    }
+};
