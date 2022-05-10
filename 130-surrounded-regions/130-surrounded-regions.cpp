ab9c42584659b7e18b270006efc26380580766bc
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-05-11 00:32:48 +0900

    Time: 39 ms (8.36%), Space: 10.2 MB (34.97%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,43 +1,117 @@
 class Solution {
 public:
-    void solve(vector<vector<char>>& board) {
-        int rowSize = board[0].size()-1;
-        for(int i = 0; i < board.size(); i++){
-            if(board[i][0] == 'O'){
-                DFS(i,0,board);
+    template <typename T>
+    void print(vector<vector<T>> &board){
+        int m=board.size();
+        int n=board[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                cout<<board[i][j]<<" ";
             }
-            if(board[i][rowSize] == 'O'){
-                DFS(i,rowSize,board);
+            cout<<endl;
+        }
+    }
+    void dfs(vector<vector<char>> &board, int i, int j, vector<vector<bool>> & visited){
+        if(visited[i][j])
+            return;
+        if(board[i][j]=='X')
+            return;
+        
+        int m=board.size();
+        int n=board[0].size();
+        visited[i][j]=true;
+        if(board[i][j]=='O')
+            board[i][j]='D';
+        for( int k = -1; k < 2; ++k )  {    
+            int dx=i+k;
+            int dy=j+k; 
+                if(dx>=0 and dx<m){
+                    if(!visited[dx][j])
+                        dfs(board, dx, j, visited);
+                }
+                if(dy>=0 and dy<n){
+                    if(!visited[i][dy])
+                        dfs(board, i, dy, visited);
+                }
+        }
+        return;
+    }
+    
+    void call_dfs(vector<vector<char>> &board, int direction, vector<vector<bool>> &visited){
+        int m=board.size();
+        int n=board[0].size();
+        int r, c;
+        if(direction==1){
+            r=0;
+            for(c=0;c<n;c++){
+               if(board[r][c]=='O')
+                   dfs(board, r, c, visited);
             }
         }
-        
-        int colSize = board.size()-1;
-        for(int i = 0; i < board[0].size(); i++){
-            if(board[0][i] == 'O'){
-                DFS(0,i,board);
+        else if(direction==2){
+            c=n-1;
+            for(r=0;r<m;r++){
+                if(board[r][c]=='O')
+                    dfs(board, r, c, visited);
             }
-            if(board[colSize][i] == 'O'){
-                DFS(colSize,i,board);
+            
+        }else if(direction==3){
+            r=m-1;
+            for(c=n-1; c>=0;c--){
+                if(board[r][c]=='O')
+                {
+                    
+                    dfs(board, r, c, visited);
+                }
             }
-        }
-        
-        for(int i = 0 ; i < board.size(); i++){
-            for(int j = 0; j < board[i].size(); j++){
-                    if(board[i][j] == '#'){
-                        board[i][j] = 'O';
-                    }else{
-                        board[i][j] = 'X';
-                    }
+            
+        }else if(direction==4){
+            c=0;
+            for(r=m-1;r>=0;r--){
+                if(board[r][c]=='O')
+                    dfs(board, r, c, visited);
             }
         }
     }
-    void DFS(int i, int j, vector<vector<char>>& board){
-        if(i>=0 && j >=0 && i <=board.size()-1 && j <= board[0].size()-1 && board[i][j] == 'O'){
-        board[i][j] = '#';
-        DFS(i+1,j,board);
-        DFS(i-1,j,board);
-        DFS(i,j+1,board);
-        DFS(i,j-1,board);
+    void capture(vector<vector<char>> &board){
+        int m=board.size();
+        int n=board[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='O')
+                    board[i][j]='X';
+                if(board[i][j]=='D')
+                    board[i][j]='O';
+            }
         }
+    }
+    void solve(vector<vector<char>>& board) {
+        int m=board.size();
+        int n=board[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n,false));
+        int direction=1;
+        call_dfs(board, direction, visited);
+        // cout<<"board after dfs direction 1"<<endl;
+        // print<char>(board);
+        // print<bool>(visited);
+        direction=2;
+        call_dfs(board, direction, visited);
+        // cout<<"board after dfs direction 2"<<endl;
+        // print<char>(board);
+        // print<bool>(visited);
+        direction=3;
+        call_dfs(board, direction, visited);
+        // cout<<"board after dfs direction 3"<<endl;
+        // print<char>(board);
+        // print<bool>(visited);
+        direction=4;
+        call_dfs(board, direction, visited);
+        // cout<<"board after dfs direction 4"<<endl;
+        // print<char>(board);
+        // print<bool>(visited);
+        capture(board);
+        // cout<<"final board"<<endl;
+        // print<char>(board);
+    
     }
 };
