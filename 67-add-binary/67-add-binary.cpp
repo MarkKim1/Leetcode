dbd6701472f98bdb2c710ec7ebff72346b51eaf0
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-05-14 23:01:09 +0900

    Time: 8 ms (18.37%), Space: 6.3 MB (82.80%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string res;
-        int i = a.length() - 1;
-        int j = b.length() - 1;
+        int i = a.size()-1;
+        int j = b.size()-1;
+        int result = 0;
         int carry = 0;
+        string ans;
         while(i >= 0 || j >= 0){
-            int sum = carry;
-            if(i >= 0) sum += a[i--] - '0';
-            if(j >= 0) sum += b[j--] - '0';
-            carry = sum > 1 ? 1 : 0;
-            res = to_string(sum % 2) + res;
+            result = carry;
+            if(i >= 0) result+=a[i--] -'0';
+            if(j >= 0) result+=b[j--] -'0';
+            carry = result > 1 ? 1 : 0;
+            ans+=to_string(result%2);
         }
-        if(carry) res = to_string(carry) +res;
-        // reverse(res.begin(), res.end());
-        return res;
+        if(carry) ans+=to_string(carry);
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
