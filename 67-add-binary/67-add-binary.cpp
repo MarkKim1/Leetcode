5c5a8180c9d14765a5c5340f561de91c1697ff67
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-05-14 22:10:39 +0900

    Time: 36 ms (5.77%), Space: 6.4 MB (54.15%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -7,10 +7,10 @@ public:
         int carry = 0;
         while(i >= 0 || j >= 0){
             int sum = carry;
-            if(i >= 0) sum += a[i--] - '0';
-            if(j >= 0) sum += b[j--] - '0';
-            carry = sum > 1 ? 1 : 0;
-            res += to_string(sum % 2);
+            if(i >= 0) sum += a[i--] - '0'; cout << sum << " ";
+            if(j >= 0) sum += b[j--] - '0'; cout << sum << " ";
+            carry = sum > 1 ? 1 : 0; cout << "carry: " << carry << " ";
+            res += to_string(sum % 2); cout << "res: " << res<< endl;
         }
         if(carry) res += to_string(carry);
         reverse(res.begin(), res.end());
