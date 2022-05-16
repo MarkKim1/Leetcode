6f90ef02ad056ac86a544e745a389c3a328d0b7d
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-05-17 01:48:50 +0900

    Time: 0 ms (100.00%), Space: 8.3 MB (76.65%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,30 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
-public:
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> result;
-        stack<TreeNode> stk;
-        TreeNode *curr = root;
-        while(curr != NULL || !(stk.empty())){
-            while(curr != NULL){
-                stk.push(*curr);
-                curr = curr->left;
-            }
-            curr = &stk.top();
-            stk.pop();
-            result.push_back(curr->val);
-            curr = curr->right;
-        }
+class Solution{
+	public:
+	vector<int> inorderTraversal(TreeNode* root){
+		vector<int> result;
+		TreeNode *curr = root;
+		while(curr){
+			if(!curr->left){
+				result.push_back(curr->val);
+				curr = curr->right;
+			}else{
+				TreeNode *pred = findPredecessor(curr);
+				if(pred->right == curr){
+					pred->right = NULL;
+					result.push_back(curr->val);
+					curr = curr->right;
+				}else{
+					pred->right = curr;
+					curr = curr->left;
+				}
+			}
+		}
         return result;
-    }
+	}
+	
+	TreeNode* findPredecessor(TreeNode *root){
+		TreeNode* curr = root->left;
+		while(curr->right && curr->right != root){
+			curr = curr->right;
+		}
+        return curr;
+	}
 };
