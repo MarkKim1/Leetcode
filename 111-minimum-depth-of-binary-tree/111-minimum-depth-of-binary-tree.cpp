6990169e467b29e2f2e977694c7be498b35cf6f8
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-05-19 01:01:37 +0900

    Time: 333 ms (46.11%), Space: 146.7 MB (6.43%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -11,35 +11,9 @@
  */
 class Solution {
 public:
-    int min_Depth = 0;
-    int result_Depth = INT_MAX;
-    queue<pair<TreeNode*, int>> q;
-    int _min(){
-        auto sub_q = q.front();
-        q.pop();
-        auto node = sub_q.first;
-        auto level = sub_q.second + 1;
-        if(node->left != NULL){
-            q.push(make_pair(node->left,level));
-        }
-        if(node->right != NULL){
-            q.push(make_pair(node->right,level));
-        }
-        if(node->right == NULL && node->left == NULL){
-            result_Depth = level;
-            return result_Depth;
-        }
-        return _min();
-        
-    }
-    
     int minDepth(TreeNode* root) {
-        if(root == NULL){
-            return 0;
-        }
-        q.push(make_pair(root,min_Depth));
-        int result = _min();
-        //sort(a.begin(),a.end());
-        return result;
+    if (!root) return 0;
+    int L = minDepth(root->left), R = minDepth(root->right);
+    return 1 + (min(L, R) ? min(L, R) : max(L, R));
     }
 };
