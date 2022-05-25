fc247614e7ce7452c4d7417f173401978b6a1a36
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-05-25 16:46:42 +0900

    Time: 16 ms (58.41%), Space: 13.3 MB (32.44%) - LeetHub

---
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+       vector<string> ans;
+        vector<pair<string,string>> sub;
+        vector<string> digit;
+        for(auto &s:logs){
+            int i = 0;
+            while(s[i] != ' ') i++;
+            if(isalpha(s[i+1])){
+                sub.push_back(make_pair(s.substr(0,i),s.substr(i+1)));
+            }else{
+                digit.push_back(s);
+            }
+        }
+        sort(sub.begin(),sub.end(),[&](auto a,auto b){
+            return a.second == b.second ? a.first < b.first : a.second < b.second;
+        });
+        for(auto a:sub){
+            ans.push_back(a.first+ ' ' + a.second);
+        }
+        for(auto a:digit){
+            ans.push_back(a);
+        }
+        return ans;
+    }
+};
