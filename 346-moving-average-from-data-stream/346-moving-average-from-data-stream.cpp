2efe905caf9afa726d82ec13504d5a0bdeb81a72
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-05-25 23:45:18 +0900

    Time: 85 ms (5.15%), Space: 13.9 MB (55.93%) - LeetHub

---
+++ 346-moving-average-from-data-stream/346-moving-average-from-data-stream.cpp
@@ -0,0 +1,38 @@
+class MovingAverage {
+public:
+    int capacity;
+    double sum = 0;
+    vector<int> ans;
+    MovingAverage(int size) {
+        capacity = size;
+    }
+    double next(int val) {
+        
+        if(ans.size() < capacity){
+            ans.push_back(val);
+            sum+=val;
+            return sum / ans.size();
+        }
+        if(ans.size() == capacity){
+            sum = 0;
+            for(int i = 0; i < ans.size(); i++){
+                if(i == ans.size()-1){
+                    ans[i] = val;
+                    break;
+                }
+                ans[i] = ans[i+1];
+            }
+            for(int i = 0; i < ans.size(); i++){
+                sum+=ans[i];
+            }
+            
+        }
+        return sum / ans.size();
+    }
+};
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage* obj = new MovingAverage(size);
+ * double param_1 = obj->next(val);
+ */
