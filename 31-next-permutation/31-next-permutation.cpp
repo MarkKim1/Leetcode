4c721109afe52b9bbe0ceda70883eb47827e53d4
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-01 21:49:39 +0900

    Time: 11 ms (19.97%), Space: 12.1 MB (37.71%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int i = nums.size()-2;
+        while(i >=0 and nums[i] >= nums[i+1]){
+            i--;
+        }
+        int j = nums.size()-1;
+        if(i >= 0){
+            while(nums[j] <= nums[i]){
+                j--;
+            }
+            swap(nums,i,j);
+        }
+        reverse(nums,i+1);
+    }
+    void swap(vector<int>&nums, int i, int j){
+        int temp;
+        temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    void reverse(vector<int>&nums,int i){
+        int j = nums.size()-1;
+        while(i < j){
+            swap(nums,i,j);
+            j--;
+            i++;
+        }
     }
 };
