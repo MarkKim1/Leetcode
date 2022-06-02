25f290296de8258e0a1bb38ee017c67c153d08d6
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-02 19:16:18 +0900

    Time: 0 ms (100.00%), Space: 6.9 MB (8.17%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -3,30 +3,29 @@ public:
     unordered_map<char,vector<char>> map = 
     {{'2',{'a','b','c'}},{'3',{'d','e','f'}},{'4',{'g','h','i'}},{'5',{'j','k','l'}},
     {'6',{'m','n','o'}},{'7',{'p','q','r','s'}},{'8',{'t','u','v'}},{'9',{'w','x','y','z'}}};
-    vector<string> combination;
     string phoneDigits;
     int a = 0;
     vector<string> letterCombinations(string digits) {
-        
         this->phoneDigits = digits;
         string letter = "";
+        vector<string> combination;
         
         if(digits.size() == 0){
             return combination;
         }
-        
-        backTracking(0,letter);
+        backTracking(0,letter,combination);
         return combination;
     }
-    void backTracking(int index,string letter){
+    void backTracking(int index,string letter,vector<string>&combination){
         if(letter.length() == phoneDigits.length()){
             combination.push_back(letter);
             return;
         }
-        vector<char> possibleLetters = map[phoneDigits[index]];
-        for(char a: possibleLetters){
+        
+        vector<char> possibleLetter = map[phoneDigits[index]];
+        for(char a: possibleLetter){
             letter+=a;
-            backTracking(index+1,letter);
+            backTracking(index+1,letter,combination);
             letter.pop_back();
         }
     }
