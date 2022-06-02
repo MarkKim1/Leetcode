e990de125125fafdd763bba812aecf38063d8494
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-03 03:19:08 +0900

    Time: 19 ms (77.03%), Space: 16.5 MB (81.18%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int jumps = 0, currentJumpEnd = 0, farthest = 0;
+        int jump = 0;
+        int currentJumpEnd = 0;
+        int farthest = 0;
         for(int i = 0; i < nums.size()-1; i++){
             farthest = max(farthest,i + nums[i]);
             
             if(i == currentJumpEnd){
-                jumps++;
-                currentJumpEnd = farthest;
+                jump++;
+                currentJumpEnd = farthest;                  
             }
         }
-        return jumps;
+        return jump;
     }
 };
