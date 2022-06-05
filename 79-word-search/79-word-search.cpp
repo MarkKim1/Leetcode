a11396a58106b69542d05414005a39d80b4c5d05
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-06 00:29:37 +0900

    Time: 921 ms (17.22%), Space: 8 MB (37.12%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
+    
     bool exist(vector<vector<char>>& board, string word) {
-    for (unsigned int i = 0; i < board.size(); i++) 
-        for (unsigned int j = 0; j < board[0].size(); j++) 
-            if (dfs(board, i, j, word))
-                return true;
-    return false;
-}
-
-bool dfs(vector<vector<char>>& board, int i, int j, string& word) {
-    if (!word.size())
-        return true;
-    if (i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[0])  
+        int index = 0;
+        for(int i = 0; i < board.size(); i++){
+            for(int j = 0; j < board[0].size(); j++){
+                if(board[i][j] == word[index])
+                    if(dfs(i,j,board,word,index))return true;
+            }
+        }
         return false;
-    char c = board[i][j];
-    board[i][j] = '*';
-    string s = word.substr(1);
-    bool ret = dfs(board, i-1, j, s) || dfs(board, i+1, j, s) || dfs(board, i, j-1, s) || dfs(board, i, j+1, s);
-    board[i][j] = c;
-    return ret;
-}
+    }
+    bool dfs(int i, int j, vector<vector<char>> &board, string word,int index){
+        if(index == word.size()){
+            return true;
+        }
+        
+        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[index]){
+            return false;
+        }
+        board[i][j] = '#';
+        if(dfs(i+1,j,board,word,index+1) ||
+           dfs(i,j+1,board,word,index+1) ||
+           dfs(i-1,j,board,word,index+1) ||
+           dfs(i,j-1,board,word,index+1)){
+            return true;
+        }
+        board[i][j] = word[index];
+        return false;
+    }
 };
