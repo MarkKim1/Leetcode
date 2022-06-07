41e60e98d96ba3985e8556988247502f641e211a
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-07 18:11:47 +0900

    Time: 0 ms (100.00%), Space: 7.7 MB (47.48%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,17 +3,19 @@ public:
     int f[101];
     int rob(vector<int>& nums) {
         int n = nums.size();
+        unordered_map<int,int> memo;
         if(n == 0) return 0;
         if(n == 1) return nums[0];
-        return dp(nums,n);
+        return dp(nums,n,memo);
     }
     
-    int dp(vector<int>& nums, int n){
-        f[0] = nums[0];
-        f[1] = max(nums[0],nums[1]);
+    int dp(vector<int>& nums, int n,unordered_map<int,int>& memo){
+        memo[0] = nums[0];
+        memo[1] = max(nums[0],nums[1]);
         for(int i = 2; i <n; i++){
-            f[i] = max(f[i-1],f[i-2] + nums[i]);
+            if(memo.find(i) == memo.end())
+                memo[i] = max(memo[i-1],memo[i-2] + nums[i]);
         }
-        return f[n-1];
+        return memo[n-1];
     }
 };
