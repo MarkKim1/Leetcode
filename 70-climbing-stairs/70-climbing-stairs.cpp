75d09c09763caaade8c60d07a954cfb16c4f5058
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-07 15:06:15 +0900

    Time: 7 ms (9.46%), Space: 6.4 MB (15.37%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
+    unordered_map<int,int> memo;
+    
     int climbStairs(int n) {
-        int f[46] = {0};
-        f[0] = 0;
-        f[1] = 1;
-        f[2] = 2;
-        for(int i = 3; i <= n; i++){
-            f[i] = f[i-1] + f[i-2];
-        }
-        return f[n];
+        // int f[46] = {0};
+        // f[0] = 0;
+        // f[1] = 1;
+        // f[2] = 2;
+        // for(int i = 3; i <= n; i++){
+        //     f[i] = f[i-1] + f[i-2];
+        // }
+        // return f[n];
+        if(memo.find(n) != memo.end()) return memo[n];
+        if(n == 0)return 1;
+        if(n < 0) return 0;
+        memo[n] = climbStairs(n-1) + climbStairs(n-2);
+        return memo[n];
     }
 };
