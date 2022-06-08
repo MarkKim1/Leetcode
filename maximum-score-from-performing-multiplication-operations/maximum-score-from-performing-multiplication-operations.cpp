97247b4cedbb8f71afa5825b6b90776103b246a2
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-08 18:14:25 +0900

    Time: 313 ms, Memory: 87.3 MB - LeetHub

---
+++ maximum-score-from-performing-multiplication-operations/maximum-score-from-performing-multiplication-operations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int n;
+    int m;
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        this->n = nums.size();
+        this->m = multipliers.size();
+        int memo[1001][1001] = {};
+        for(int i = m-1; i >= 0; i--){
+            for(int left = i; left >= 0; left--){
+                int right = n - 1 - (i - left);
+                memo[i][left] = max(multipliers[i] * nums[left] + memo[i+1][left+1],
+                                    multipliers[i] * nums[right] + memo[i+1][left]);
+            }
+        }
+        
+        return memo[0][0];
+    }
+};
