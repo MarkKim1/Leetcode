061cad8e77c8bdb00ba6ae094e70bfc8ffb78319
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-08 17:55:37 +0900

    Time: 312 ms (98.62%), Space: 87.3 MB (79.82%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     int n;
     int m;
-    int memo[1001][1001] = {};
     int maximumScore(vector<int>& nums, vector<int>& multipliers) {
         this->n = nums.size();
         this->m = multipliers.size();
-        return dp(0,0,nums,multipliers);
-    }
-    
-    int dp(int i, int left,vector<int>& num, vector<int>& multi){
-        if(i == m) return 0;
-        int mult = multi[i];
-        int right = n - 1 - (i - left);
-        if(memo[i][left] == 0){
-        memo[i][left] = max(mult * num[left] + dp(i+1,left+1,num,multi),
-                            mult * num[right] + dp(i+1,left,num,multi));
+        int memo[1001][1001] = {};
+        
+        for(int i = m-1; i >= 0; i--){
+            for(int left = i; left >= 0; left--){
+                int right = n - 1 - (i - left);
+                memo[i][left] = max(multipliers[i] * nums[left] + memo[i+1][left+1],
+                                    multipliers[i] * nums[right] + memo[i+1][left]);
+            }
         }
-        return memo[i][left];
+        
+        return memo[0][0];
     }
 };
