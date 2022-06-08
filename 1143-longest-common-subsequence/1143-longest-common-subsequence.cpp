4bd7828bb8f1816226165980bed0a94d1ba7580a
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-08 20:07:34 +0900

    Time: 34 ms (45.37%), Space: 10.8 MB (60.65%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
+    string text1;
+    string text2;
+    int answer;
+    int memo[1001][1001];
     int longestCommonSubsequence(string text1, string text2) {
-        int memo[1001][1001] = {0};
-        for(int row = text1.size()-1;row>= 0; row--){
-            for(int col = text2.size()-1; col>=0; col--){
-                if(text1[row] == text2[col]){
-                    memo[row][col] = 1 + memo[row+1][col+1];
-                }else{
-                    memo[row][col] = max(memo[row][col+1],memo[row+1][col]);
-                }
+        this->text1 = text1;
+        this->text2 = text2;
+        
+        for (int i = 0; i < text1.length(); i++) {
+            for (int j = 0; j < text2.length(); j++) {
+                this->memo[i][j] = -1;
             }
         }
-        return memo[0][0];
+        this->answer = 0;
+        return dp(0,0);
+    }
+    int dp(int p1,int p2){
+        if(memo[p1][p2] != -1){
+            return memo[p1][p2];
+        }
+        if(text1[p1] == text2[p2]){
+            answer = 1 + dp(p1+1,p2+1);
+        }
+        else{
+            answer = max(dp(p1,p2+1),dp(p1+1,p2));
+        }
+        
+        memo[p1][p2] = answer;
+        return memo[p1][p2];
     }
 };
