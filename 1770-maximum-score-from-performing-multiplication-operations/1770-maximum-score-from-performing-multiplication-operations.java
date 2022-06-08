b3a1799f63d0fec7e46b8e0b07183fde423a1066
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-08 17:51:23 +0900

    Time: 68 ms (73.71%), Space: 120.2 MB (60.15%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,31 +1,18 @@
 class Solution {
-    private int[][] memo;
-    private int[] nums, multipliers;
-    private int n, m;
-    
-    private int dp(int i, int left) {
-        if (i == m) {
-            return 0; // Base case
-        }
-
-        int mult = multipliers[i];
-        int right = n - 1 - (i - left);
-            
-        if (memo[i][left] == 0) {
-            // Recurrence relation
-            memo[i][left] = Math.max(mult * nums[left] + dp(i + 1, left + 1), 
-                                     mult * nums[right] + dp(i + 1, left));
-        }
-
-        return memo[i][left];
-    }
-    
     public int maximumScore(int[] nums, int[] multipliers) {
-        n = nums.length;
-        m = multipliers.length;
-        this.nums = nums;
-        this.multipliers = multipliers;
-        this.memo = new int[m][m];
-        return dp(0, 0);
+        int n = nums.length;
+        int m = multipliers.length;
+        int[][] dp = new int[m + 1][m + 1];
+        
+        for (int i = m - 1; i >= 0; i--) {
+            for (int left = i; left >= 0; left--) {
+                int mult = multipliers[i];
+                int right = n - 1 - (i - left);
+                dp[i][left] = Math.max(mult * nums[left] + dp[i + 1][left + 1], 
+                                       mult * nums[right] + dp[i + 1][left]);
+            }
+        }
+        
+        return dp[0][0];
     }
 }
