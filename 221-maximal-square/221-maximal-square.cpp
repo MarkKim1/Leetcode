a40bc1687a11344e7f0bec04e137b2199161ec12
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-08 21:54:14 +0900

    Time: 136 ms (18.04%), Space: 16.7 MB (75.00%) - LeetHub

---
+++ 221-maximal-square/221-maximal-square.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int dp[301][301] = {0};
+        int area = 0;
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[0].size(); j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = matrix[i][j] == '1' ? 1: 0;
+                    area = max(area,dp[i][j]);
+                }else if(matrix[i][j] == '1'){
+                    dp[i][j] = min(dp[i][j-1],min(dp[i-1][j-1],dp[i-1][j])) + 1;
+                    area = max(area,dp[i][j]);
+                }
+            }
+        }
+        return area*area;
+    }
+};
