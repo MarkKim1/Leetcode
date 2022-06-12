633c02ca8a5bffa95c034e0a5cf96981f165aaf6
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-12 23:17:03 +0900

    Time: 15 ms (21.58%), Space: 45.8 MB (14.57%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,27 +1,23 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int dp[][][] = new int[n + 1][k + 1][2];
+        int[][][] dp = new int[n+1][k+1][2];
         
-        for (int i = n - 1; i >= 0; i--) {
-            for (int transactionsRemaining = 1; transactionsRemaining <= k; transactionsRemaining++) {
-                for (int holding = 0; holding < 2; holding++) {
-                    int doNothing = dp[i + 1][transactionsRemaining][holding];
+        for(int i = n-1; i >= 0; i--){
+            for(int transactionRemaining = 1; transactionRemaining <= k; transactionRemaining++){
+                for(int holding = 0; holding < 2; holding++){
+                    int doNothing = dp[i+1][transactionRemaining][holding];
                     int doSomething;
-                    if (holding == 1) {
-                        // Sell stock
-                        doSomething = prices[i] + dp[i + 1][transactionsRemaining - 1][0];
-                    } else {
-                        // Buy stock
-                        doSomething = -prices[i] + dp[i + 1][transactionsRemaining][1];
-                    }
                     
-                    // Recurrence relation
-                    dp[i][transactionsRemaining][holding] = Math.max(doNothing, doSomething);
+                    if(holding == 1){//sell stock
+                        doSomething = prices[i] + dp[i+1][transactionRemaining-1][0];
+                    }else{//buy stock
+                        doSomething = -prices[i] + dp[i+1][transactionRemaining][1];
+                    }
+                    dp[i][transactionRemaining][holding] = Math.max(doNothing,doSomething);
                 }
             }
         }
-        
         return dp[0][k][0];
     }
 }
