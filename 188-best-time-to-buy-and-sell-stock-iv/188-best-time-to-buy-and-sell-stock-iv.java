08667c3d5020fbf19ddcf11d3a3f9ec0929ffc62
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-12 23:05:47 +0900

    Time: 30 ms (13.88%), Space: 44.9 MB (23.64%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,29 +1,27 @@
 class Solution {
-    int[] prices;
-    int[][][] memo;
     public int maxProfit(int k, int[] prices) {
-        this.prices = prices;
-        this.memo = new int[prices.length][k+1][2];
-        return dp(0,k,0);
-    }
-    
-    private int dp(int i, int tR, int holding){
-        if(tR == 0 || i == prices.length){
-            return 0;
-        }
-        if(memo[i][tR][holding] == 0){
-            int doNothing = dp(i+1,tR,holding);
-            int doSomething;
-            
-            if(holding == 1){
-                //sell stock
-                doSomething = prices[i] + dp(i+1,tR-1,0); 
-            }else{
-                //buy stock
-                doSomething = -prices[i] + dp(i+1,tR,1);
+        int n = prices.length;
+        int dp[][][] = new int[n + 1][k + 1][2];
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int transactionsRemaining = 1; transactionsRemaining <= k; transactionsRemaining++) {
+                for (int holding = 0; holding < 2; holding++) {
+                    int doNothing = dp[i + 1][transactionsRemaining][holding];
+                    int doSomething;
+                    if (holding == 1) {
+                        // Sell stock
+                        doSomething = prices[i] + dp[i + 1][transactionsRemaining - 1][0];
+                    } else {
+                        // Buy stock
+                        doSomething = -prices[i] + dp[i + 1][transactionsRemaining][1];
+                    }
+                    
+                    // Recurrence relation
+                    dp[i][transactionsRemaining][holding] = Math.max(doNothing, doSomething);
+                }
             }
-            memo[i][tR][holding] = Math.max(doNothing,doSomething);
         }
-        return memo[i][tR][holding];
+        
+        return dp[0][k][0];
     }
 }
