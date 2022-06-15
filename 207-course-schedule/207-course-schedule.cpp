2c6eb418cc3efdadf7a39f0064b8b0b1234fe3c4
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-15 19:52:09 +0900

    Time: 34 ms (42.11%), Space: 13.4 MB (75.43%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
-    
-    //detct the cycle in graph 
-    bool iscycle(vector<int> adj[], vector<int>&visit,int node){
-        if(visit[node]==1){
-            return true; 
+    bool canFinish(int numCourses, vector<vector<int>>& pre) {
+        vector<vector<int>> result(numCourses);
+        vector<int> visited(numCourses,0);
+        for(int i = 0; i < pre.size(); i++){
+            result[pre[i][0]].push_back(pre[i][1]);
         }
-        
-        if(visit[node]==0){
-            visit[node]= 1;
-            for(auto x: adj[node]){
-                if(iscycle(adj,visit,x)){
-                    return true;
-                }
-            }
-        }
-        
-        visit[node]=2;
-        return false;
-        
-    }
-    
-    
-    bool canFinish(int numc, vector<vector<int>>& pqt) {
-       // unordered_map<int,vector<int>>adj;
-        //graph is ready 
-        vector<int>adj[numc];
-        
-        for(auto x: pqt){
-             adj[x[1]].push_back(x[0]);
-        }
-        
-        //visit vector
-        vector<int>visit(numc,0);
-        
-        for(int i =0; i<numc; i++){
-            if(iscycle(adj,visit,i)){
+        for(int i = 0; i < numCourses; i++){
+            if(visited[i] == 0){
+                if(isCycle(result,visited,i))
                 return false;
             }
         }
         return true;
     }
+    bool isCycle(vector<vector<int>>& result,vector<int>& visited,int curr){
+        if(visited[curr] == 2){
+            return true;
+        }
+        visited[curr] = 2;
+        for(int i = 0; i < result[curr].size(); i++){
+            if(visited[result[curr][i]] != 1){
+                if(isCycle(result,visited,result[curr][i])){
+                    return true;
+                }      
+            }  
+        }
+        visited[curr] = 1;
+        return false;
+    }
+    
 };
