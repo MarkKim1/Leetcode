dac96a8e4decccbfe97906f11bb17cfb9b463b03
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-17 07:03:43 +0900

    Time: 28 ms (22.84%), Space: 13.2 MB (81.83%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -16,6 +16,7 @@ public:
                 }
             }
         }
+        
         for(int i = 1; i< R; i++){
             for(int j = 1; j < C; j++){
                 if(matrix[0][j] == 0|| matrix[i][0] == 0){
@@ -23,11 +24,13 @@ public:
                 }
             }
         }
+        
         if(matrix[0][0] == 0){
             for(int j = 0; j < C; j++){
                 matrix[0][j] = 0;
             }
         }
+        
         if(isCol){
             for(int i = 0; i < R; i++){
                 matrix[i][0] = 0;
