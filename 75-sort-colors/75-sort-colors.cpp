4a8b35a1d27eb48cd8d2e55802bd51bc8a4b8f06
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-17 21:18:00 +0900

    Time: 6 ms (21.87%), Space: 8.3 MB (70.25%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int pos1 = 0,curr = 0;
+        int pos2 = nums.size()-1;
         
+        while(curr <= pos2){
+            if(nums[curr] == 0){
+                swap(nums[curr++],nums[pos1++]);
+            }
+            else if(nums[curr] == 2){
+                swap(nums[curr],nums[pos2--]);
+            }else{
+                curr++;
+            }
+        }
     }
 };
