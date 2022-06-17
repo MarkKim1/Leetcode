441557f2c58535e088c3bcc888f227a459e14de5
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-17 20:27:44 +0900

    Time: 383 ms (5.06%), Space: 92.9 MB (11.86%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.cpp
+++ 739-daily-temperatures/739-daily-temperatures.cpp
@@ -5,14 +5,11 @@ public:
         stack<pair<int,int>> stk;
         int temp = temperatures[0];
         stk.push(make_pair(temp,0));
-        int i = 1;
-        for(int i = 0; i < temperatures.size(); i++){
-            //if(temperatures[i] > stk.top().first){
-                while(!stk.empty() and stk.top().first < temperatures[i]){
-                    answer[stk.top().second] = i-stk.top().second;
-                    stk.pop();
-                }
-            //}
+        for(int i = 1; i < temperatures.size(); i++){
+            while(!stk.empty() and stk.top().first < temperatures[i]){
+                answer[stk.top().second] = i-stk.top().second;
+                stk.pop();
+            }
             stk.push(make_pair(temperatures[i],i));
         }
         return answer;
