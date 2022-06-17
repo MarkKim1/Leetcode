ed81b853e2f39068e507ddb15474a986fd952a1d
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-17 09:28:07 +0900

    Time: 8 ms (66.99%), Space: 6.2 MB (80.66%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        //there are two base cases
-        //1. when s is single character
-        //2. when s is double letter string
-        
-        if(s.size() == 0) return 0;
-        
-        int n = s.size();
         int ans = 0;
-        bool dp[n][n];
-        fill_n(*dp,n*n,false);
-        
-        //1. when s is single character
-        for(int i = 0; i < n; i++){
-            dp[i][i] = true;
-            ans++;
+        for(int i = 0; i < s.size(); i++){
+            //odd-length palindromes, single character center
+            ans+=is_palindrome(s,i,i);
+            //even-length palindromes, consecutive character center
+            ans+=is_palindrome(s,i,i+1);
         }
-        //2. when s is double letter string
-        for(int i = 0; i <n-1; i++){
-            dp[i][i+1] = s[i] == s[i+1];
-            ans+=dp[i][i+1];
-        }
-        
-        //for string from 3 to n
-        for(int len = 3; len <= n; len++){
-            for(int i = 0,j = i+len-1;j<n;i++,j++){
-                dp[i][j] = dp[i+1][j-1] && (s[i]==s[j]);
-                ans+=dp[i][j];
+        return ans;
+    }
+    int is_palindrome(const string& s,int low,int high){
+        int ans = 0;
+        while(low >= 0 and high < s.size()){
+            if(s[low] != s[high]){
+                break;
             }
+            low--;
+            high++;
+            ans++;
         }
         return ans;
     }
