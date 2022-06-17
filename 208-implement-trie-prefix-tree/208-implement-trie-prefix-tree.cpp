11bddc5c1c146efacc87ca331f8c580736a7dd59
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-17 22:05:38 +0900

    Time: 78 ms (70.14%), Space: 33.8 MB (97.70%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.cpp
@@ -1,35 +1,60 @@
+class TrieNode {
+    public:
+        map<char,TrieNode*> next;
+        bool end = false;
+};
 class Trie {
 public:
-    Trie() {}
-
+    TrieNode* node;
+    Trie() {
+        node = new TrieNode();
+    }
+    
     void insert(string word) {
-        Trie* node = this;
-        for (char ch : word) {
-            if (!node->next.count(ch)) { node->next[ch] = new Trie(); }
-            node = node->next[ch];
+        TrieNode* root = node;
+        for(auto ch:word) {
+            if(root->next.count(ch)) {
+                root = root->next[ch];
+            } else {
+                root->next[ch] = new TrieNode();
+                root = root->next[ch];
+            }
+            
         }
-        node->isword = true;
+        
+        root->end = true;     
+        
     }
-
+    
     bool search(string word) {
-        Trie* node = this;
-        for (char ch : word) {
-            if (!node->next.count(ch)) { return false; }
-            node = node->next[ch];
+        TrieNode* root = node;
+        for(auto ch:word) {
+            if(root->next.count(ch)) {
+                root = root->next[ch];
+            }  else {
+                return false;
+            }
         }
-        return node->isword;
+        return root->end;
     }
-
+    
     bool startsWith(string prefix) {
-        Trie* node = this;
-        for (char ch : prefix) {
-            if (!node->next.count(ch)) { return false; }
-            node = node->next[ch];
+        TrieNode* root = node;
+        for(auto ch:prefix) {
+            if(root->next.count(ch)) {
+                root = root->next[ch];
+            }  else {
+                return false;
+            }
         }
         return true;
     }
-
-private:
-    map<char, Trie*> next = {};
-    bool isword = false;
 };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
