19c529f697c6cf625f51ed2249f2b720974e387f
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-17 09:03:43 +0900

    Time: 15 ms (55.25%), Space: 7.2 MB (59.24%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,31 +1,35 @@
 class Solution {
- public:
+public:
     int countSubstrings(string s) {
-        int n = s.size(), ans = 0;
-
-        if (n <= 0)
-            return 0;
-
+        //there are two base cases
+        //1. when s is single character
+        //2. when s is double letter string
+        
+        if(s.size() == 0) return 0;
+        
+        int n = s.size();
+        int ans = 0;
         bool dp[n][n];
-        fill_n(*dp, n * n, false);
-
-        // Base case: single letter substrings
-        for (int i = 0; i < n; ++i, ++ans)
+        fill_n(*dp,n*n,false);
+        
+        //1. when s is single character
+        for(int i = 0; i < n; i++){
             dp[i][i] = true;
-
-        // Base case: double letter substrings
-        for (int i = 0; i < n - 1; ++i) {
-            dp[i][i + 1] = (s[i] == s[i + 1]);
-            ans += dp[i][i + 1];
+            ans++;
         }
-
-        // All other cases: substrings of length 3 to n
-        for (int len = 3; len <= n; ++len)
-            for (int i = 0, j = i + len - 1; j < n; ++i, ++j) {
-                dp[i][j] = dp[i + 1][j - 1] && (s[i] == s[j]);
-                ans += dp[i][j];
+        //2. when s is double letter string
+        for(int i = 0; i <n-1; i++){
+            dp[i][i+1] = s[i] == s[i+1];
+            ans+=dp[i][i+1];
+        }
+        
+        //for string from 3 to n
+        for(int len = 3; len <= n; len++){
+            for(int i = 0,j = i+len-1;j<n;i++,j++){
+                dp[i][j] = dp[i+1][j-1] && (s[i]==s[j]);
+                ans+=dp[i][j];
             }
-
+        }
         return ans;
     }
 };
