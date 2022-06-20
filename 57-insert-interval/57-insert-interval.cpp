8db08b6ea9b1675fd9a8e6067a7a35adc96d389c
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-21 06:50:32 +0900

    Time: 27 ms (41.88%), Space: 17.1 MB (38.50%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> result;
+        if(intervals.size() == 0){
+            result.push_back(newInterval);
+            return result;
+        }
+        
+        for(int i = 0; i < intervals.size(); i++){
+            if(intervals[i][0] > newInterval[1]){
+                result.push_back(newInterval);
+                newInterval = intervals[i];
+            }
+            else if(intervals[i][1] < newInterval[0]){
+                result.push_back(intervals[i]);
+            }
+            else if(intervals[i][1] >= newInterval[0] || intervals[i][0] <= newInterval[1]){
+                newInterval[0] = min(intervals[i][0],newInterval[0]);
+                newInterval[1] = max(intervals[i][1],newInterval[1]);
+            }
+        }
+        result.push_back(newInterval);
+        return result;
+    }
+};
