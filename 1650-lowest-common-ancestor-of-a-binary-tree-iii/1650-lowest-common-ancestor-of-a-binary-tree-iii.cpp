c08da652bf11d60c0ef7ab005bd2f3aaeb2a12d8
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-20 15:21:29 +0900

    Time: 601 ms (5.23%), Space: 10.2 MB (63.60%) - LeetHub

---
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -0,0 +1,25 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* parent;
+};
+*/
+
+class Solution {
+public:
+    Node* lowestCommonAncestor(Node* p, Node * q) {
+        Node* a = p;
+        Node* b = q;
+        
+        while(a != b){
+            a = a==nullptr ? p : a->parent;
+            b = b==nullptr ? q : b->parent;
+            
+        }
+        return a;
+    }
+};
