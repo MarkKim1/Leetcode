e308867823016e213e738ce6ef4927ef28fc79e8
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-22 14:56:46 +0900

    Time: 13 ms (25.00%), Space: 9 MB (31.13%) - LeetHub

--- 133-clone-graph/133-clone-graph.cpp
+++ 133-clone-graph/133-clone-graph.cpp
@@ -1,40 +1,38 @@
-"""
-# Definition for a Node.
-class Node(object):
-    def __init__(self, val, neighbors):
-        self.val = val
-        self.neighbors = neighbors
-"""
-class Solution(object):
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
 
-    def __init__(self):
-        # Dictionary to save the visited node and it's respective clone
-        # as key and value respectively. This helps to avoid cycles.
-        self.visited = {}
-
-    def cloneGraph(self, node):
-        """
-        :type node: Node
-        :rtype: Node
-        """
-        if not node:
-            return node
-
-        # If the node was already visited before.
-        # Return the clone from the visited dictionary.
-        if node in self.visited:
-            return self.visited[node]
-
-        # Create a clone for the given node.
-        # Note that we don't have cloned neighbors as of now, hence [].
-        clone_node = Node(node.val, [])
-
-        # The key is original node and value being the clone node.
-        self.visited[node] = clone_node
-
-        # Iterate through the neighbors to generate their clones
-        # and prepare a list of cloned neighbors to be added to the cloned node.
-        if node.neighbors:
-            clone_node.neighbors = [self.cloneGraph(n) for n in node.neighbors]
-
-        return clone_node
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(!node){
+            return NULL;
+        }
+        if(map.find(node) == map.end()){
+            map[node] = new Node(node->val,vector<Node*>());
+            for(auto adj : node->neighbors){
+                map[node]->neighbors.push_back(cloneGraph(adj));
+            }
+        }
+        return map[node];
+    }
+private:
+    unordered_map<Node*,Node*> map;
+};
