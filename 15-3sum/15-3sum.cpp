480fd3ba44fde86f4b1a1e23534534d900220cbf
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-22 10:37:28 +0900

    Time: 39 ms (99.95%), Space: 19.9 MB (96.39%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,31 +1,51 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> result;
-        if(nums.size() < 3) return result;
+        if (nums.size() < 3) {
+            //throw invalid_argument("num.size is less than 3");
+            return vector<vector<int>>();
+        }
         
-        for(int i = 0; i < nums.size()-1; i++){
-            int first = nums[i];
-            if(i == 0 || nums[i] != nums[i-1]){
-                int left = i+1;
-                int right = nums.size()-1;
-                while(left < right){
-                    if(first + nums[left] + nums[right] == 0){
-                        vector<int> sub = {first,nums[left],nums[right]};
-                        result.push_back(sub);
-                        while(left < right and nums[left] == nums[left+1]) left++;
-                        while(left < right and nums[right] == nums[right-1]) right--;
-                        left++;
-                        right--;
-                    }else if(first + nums[left] + nums[right] < 0){
-                        left++;
-                    }else if(first + nums[left] + nums[right] > 0){
-                        right--;
+        sort(nums.begin(), nums.end());
+        
+        vector<vector<int>> results;
+        
+        auto i = nums.begin();
+        int target = *i - 1;
+        for (; i != nums.end() - 2; ++i) {
+            if (*i > 0 || *i == target) { continue; }
+            target = *i;
+            
+            // 左右双指针向中间遍历
+            auto j = i + 1;
+            auto k = nums.end() - 1;
+            
+            while (j < k) {
+                if (*j + *k > -target) {
+                    --k;
+                    while (j < k && *k == *(k+1)) {
+                        --k;
+                    }
+                } else if (*j + *k < -target) {
+                    ++j;
+                    while (j < k && *j == *(j-1)) {
+                        ++j;
+                    }
+                } else {
+                    results.push_back( vector<int>{*i, *j, *k} );
+                    ++j;
+                    while (j < k && *j == *(j-1)) {
+                        ++j;
+                    }
+                    --k;
+                    while (j < k && *k == *(k+1)) {
+                        --k;
                     }
                 }
             }
+            
         }
-        return result;
+        
+        return results;
     }
 };
