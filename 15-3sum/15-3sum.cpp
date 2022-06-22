aedce1c28f1d5b3e6b95fbdb3a6c739e66ff7ba1
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-22 10:36:12 +0900

    Time: 245 ms (24.23%), Space: 21.1 MB (49.08%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>>res;
-        for(int i = 0; i < nums.size() && nums[i] <= 0; i++){
-            if(i == 0 || nums[i-1] != nums[i]){
-                twoSum(nums,i,res);
-            }
-        }
-        return res;
-    }
-    void twoSum(vector<int>&nums,int i, vector<vector<int>> &res){
-        unordered_set<int> seen;
-        for(int j = i+1; j < nums.size(); j++){
-            int complement = -nums[i] - nums[j];
-            if(seen.count(complement)){
-                res.push_back({nums[i],complement, nums[j]});
-                while(j+1 < nums.size() && nums[j] == nums[j+1]){
-                    ++j;
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> result;
+        if(nums.size() < 3) return result;
+        
+        for(int i = 0; i < nums.size()-1; i++){
+            int first = nums[i];
+            if(i == 0 || nums[i] != nums[i-1]){
+                int left = i+1;
+                int right = nums.size()-1;
+                while(left < right){
+                    if(first + nums[left] + nums[right] == 0){
+                        vector<int> sub = {first,nums[left],nums[right]};
+                        result.push_back(sub);
+                        while(left < right and nums[left] == nums[left+1]) left++;
+                        while(left < right and nums[right] == nums[right-1]) right--;
+                        left++;
+                        right--;
+                    }else if(first + nums[left] + nums[right] < 0){
+                        left++;
+                    }else if(first + nums[left] + nums[right] > 0){
+                        right--;
+                    }
                 }
             }
-            seen.insert(nums[j]);
         }
+        return result;
     }
 };
