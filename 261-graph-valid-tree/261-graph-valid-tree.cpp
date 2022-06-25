25979c493b512b681c93298467493bbfed9e7ad9
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-26 08:20:13 +0900

    Time: 45 ms (14.99%), Space: 11.9 MB (95.74%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.cpp
+++ 261-graph-valid-tree/261-graph-valid-tree.cpp
@@ -3,18 +3,24 @@ public:
     bool validTree(int n, vector<vector<int>>& edges) {
         if(n == 0) return true;
         if(n-1 != edges.size()) return false;
-        vector<int> a(n,0);
-        for(int i = 0; i < n; i ++){
-            a[i] = i;
+        vector<int> nodes(n,0);
+        for(int i = 0; i < n; i++){
+            nodes[i] = i;
         }
+        
         for(int i = 0; i < edges.size(); i++){
-            int first = edges[i][0];
-            int second = edges[i][1];
-            while(first != a[first]) first = a[first];
-            while(second != a[second]) second = a[second];
-            a[second] = first;
-            if(first == second) return false;
+            int A = find(edges[i][0],nodes);
+            int B = find(edges[i][1],nodes);
+            nodes[A] = B;
+            if(A == B) return false;
         }
         return true;
+    }
+    
+    int find(int a,vector<int>& nodes){
+        if(a == nodes[a]){
+            return nodes[a];
+        }
+        return a = find(nodes[a],nodes);
     }
 };
