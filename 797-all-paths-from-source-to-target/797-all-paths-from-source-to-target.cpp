1ebb257b12ab43a6adf92f554563520b0a2757e6
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-06-30 02:49:18 +0900

    Time: 31 ms (39.55%), Space: 10.7 MB (83.27%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<vector<int>> result;
-        vector<int> sub;
-        int size = graph.size()-1;
-        dfs(0,graph,sub,size,result);
-        return result;
+        void dfs(vector<vector<int>>& g, vector<vector<int>>& res, vector<int>& path, int cur) {
+        path.push_back(cur);
+        if (cur == g.size() - 1)
+            res.push_back(path);
+        else for (auto it: g[cur])
+            dfs(g, res, path, it);
+        path.pop_back();
+
     }
-     void dfs(int i, vector<vector<int>>& graph, vector<int> sub,int size,vector<vector<int>>& result){
-         if(i == size) {
-             sub.push_back(size);
-             result.push_back(sub);
-             return;
-         }
-         sub.push_back(i);
-         for(int j = 0; j < graph[i].size(); j++){
-             dfs(graph[i][j],graph,sub,size,result);
-         }
-         sub.pop_back();
+
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& g) {
+        vector<vector<int>> paths;
+        vector<int> path;
+        dfs(g, paths, path, 0);
+        return paths;
     }
 };
