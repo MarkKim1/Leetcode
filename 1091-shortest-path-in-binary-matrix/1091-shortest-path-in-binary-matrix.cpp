3d8d8e8f7374a6a0c3271958cf2a45ad46e5ebaf
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-04 08:20:34 +0900

    Time: 158 ms (28.58%), Space: 26.5 MB (31.82%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,47 +1,45 @@
 class Solution {
-    vector<vector<int>> directions = 
-        {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-    public:
+public:
+    vector<vector<int>> directions = {{-1,-1},{-1,0},{-1,1},{0,-1},{0,1},{1,-1},{1,0},{1,1}};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        
-        if(grid[0][0] != 0|| grid[grid.size()-1][grid[0].size()-1] != 0){
+        if(grid[0][0] != 0 || grid[grid.size()-1][grid[0].size()-1] != 0){
             return -1;
         }
-        
         queue<pair<int,int>> q;
-        grid[0][0] = 1;
         q.push(make_pair(0,0));
+        grid[0][0] = 1;
         
-        while(!(q.empty())){
+        while(!q.empty()){
             auto it = q.front();
             q.pop();
             int row = it.first;
             int col = it.second;
-            int distance = grid[row][col];
-            if(row == grid.size()-1 && col == grid[0].size()-1){
-                return distance;
-            }
-            for(auto neighbour: getNeighbours(row,col,grid)){
-                int neighbourRow = neighbour.first;
-                int neighborCol = neighbour.second;
-                q.push(make_pair(neighbourRow,neighborCol));
-                grid[neighbourRow][neighborCol] = distance + 1;
+            for(auto neighbor: neighbors(row,col,grid)){
+                int newRow = neighbor.first;
+                int newCol = neighbor.second;
+                grid[newRow][newCol] = grid[row][col] + 1;
+                q.push(make_pair(newRow,newCol));
             }
         }
-        return -1;
+        if(grid[grid.size()-1][grid[0].size()-1] != 0){
+            return grid[grid.size()-1][grid[0].size()-1];
+        }else{
+            return -1;
+        }
     }
-    vector<pair<int,int>> getNeighbours(int row,int col,vector<vector<int>>&grid){
-        vector<pair<int,int>> neighbours;
+    vector<pair<int,int>> neighbors(int row,int col, vector<vector<int>>& grid){
+        vector<pair<int,int>> result;
         for(int i = 0; i < directions.size(); i++){
-            int newRow = row + directions[i][0];
-            int newCol = col + directions[i][1];
-            if(newRow < 0 || newCol < 0 || newRow >= grid.size() || newCol >= grid[0].size()
-              || grid[newRow][newCol] != 0){
+            if(row + directions[i][0] < 0 || col + directions[i][1] < 0 || row + directions[i][0] >= grid.size() || col + directions[i][1] >= grid[0].size()){
                 continue;
             }
-            neighbours.push_back(make_pair(newRow,newCol));
+            int newRow = row + directions[i][0];
+            int newCol = col + directions[i][1];
+            if(grid[newRow][newCol] == 0){
+                result.push_back(make_pair(newRow,newCol));
+            }
         }
-        return neighbours;
+        return result;
     }
     
 };
