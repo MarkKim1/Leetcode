32e92046ef7741f92da206661ec7618aba3f3038
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-04 10:39:00 +0900

    Time: 19 ms (98.10%), Space: 12 MB (9.18%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -21,22 +21,26 @@ public:
 class Solution {
 public:
     vector<vector<int>> levelOrder(Node* root) {
-        if(root == nullptr) return {};
-        deque<Node*> child(1,root);
-        vector<vector<int>> levelOrder({{root->val}});
-        while(!child.empty()){
-            vector<int> oneLevel;
-            int size = child.size();
-                 for(int i=0; i<size; ++i){
-                     for(auto & children: child[0]->children){
-                         child.push_back(children);
-                         oneLevel.push_back(children->val);
-                     }
-                     child.pop_front();
-                 }
-            levelOrder.push_back(move(oneLevel));
+        queue<Node*> q;
+        q.push(root);
+        vector<vector<int>> result;
+        if(root == NULL) return result;
+        vector<int> sub;
+        while(!q.empty()){
+            int size = q.size();
+            
+            for(int i = 0; i < size; i++){
+                sub.push_back(q.front()->val);
+                vector<Node*> a = q.front()->children;
+                
+                for(int i = 0; i < a.size(); i++){
+                    q.push(a[i]);
+                }
+                q.pop();   
+            }
+            result.push_back(sub);
+            sub.clear();
         }
-        levelOrder.pop_back();
-        return levelOrder;
+        return result;
     }
 };
