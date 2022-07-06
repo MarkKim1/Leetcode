234fd45c620451ba427095e8b1c1f5c55aaec723
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-06 21:49:21 +0900

    Time: 217 ms (35.32%), Space: 43.5 MB (39.96%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,40 +1,61 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>> adj(n+1);
-        for(vector<int> time: times){
-            int from = time[0];
-            int to = time[1];
-            int distance = time[2];
-            adj[from].push_back({distance,to});
-        }
-        vector<int> signal_sum(n+1,INT_MAX);
-        int answer = INT_MIN;
-        BFS(adj,k,signal_sum);
-        signal_sum[k] = 0;
-        for(int i = 1; i <=n; i++){
-            answer = max(answer,signal_sum[i]);
-        }
-        return answer == INT_MAX ? -1: answer;
-    }
-    void BFS(vector<vector<pair<int,int>>>& adj, int k,vector<int>& signal_sum){
-        priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>>pq;
-        pq.push({0,k});//pq.first = distance, pq.second = node;
-        while(!pq.empty()){
-            int currnode = pq.top().second;
-            int weight = pq.top().first;
+    // Adjacency list, defined it as per the maximum number of nodes
+    // But can be defined with the input size as well
+    vector<pair<int, int>> adj[101];
+    
+    void dijkstra(vector<int>& signalReceivedAt, int source, int n) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, 
+        greater<pair<int, int>>> pq;
+        pq.push({0, source});
+        
+        // Time for starting node is 0
+        signalReceivedAt[source] = 0;
+        
+        while (!pq.empty()) {
+            int currNodeTime = pq.top().first;
+            int currNode = pq.top().second; 
             pq.pop();
-            if (weight > signal_sum[currnode]) {
+            
+            if (currNodeTime > signalReceivedAt[currNode]) {
                 continue;
             }
-            for(auto neighborNode: adj[currnode]){
-                int newWeight = neighborNode.first;
-                int nextNode = neighborNode.second;
-                if(signal_sum[nextNode] > weight + newWeight){
-                    signal_sum[nextNode] = weight + newWeight;
-                    pq.push({weight + newWeight,nextNode});
+            
+            // Broadcast the signal to adjacent nodes
+            for (pair<int, int> edge : adj[currNode]) {
+                int time = edge.first;
+                int neighborNode = edge.second;
+                
+                // Fastest signal time for neighborNode so far
+                // signalReceivedAt[currNode] + time : 
+                // time when signal reaches neighborNode
+                if (signalReceivedAt[neighborNode] > currNodeTime + time) {
+                    signalReceivedAt[neighborNode] = currNodeTime + time;
+                    pq.push({signalReceivedAt[neighborNode], neighborNode});
                 }
             }
         }
+    }
+    
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        // Build the adjacency list
+        for (vector<int> time : times) {
+            int source = time[0];
+            int dest = time[1];
+            int travelTime = time[2];
+            
+            adj[source].push_back({travelTime, dest});
+        }
+        
+        vector<int> signalReceivedAt(n + 1, INT_MAX);
+        dijkstra(signalReceivedAt, k, n);
+        
+        int answer = INT_MIN;
+        for (int i = 1; i <= n; i++) {
+            answer = max(answer, signalReceivedAt[i]);
+        }
+        
+        // INT_MAX signifies atleat one node is unreachable
+        return answer == INT_MAX ? -1 : answer;
     }
 };
