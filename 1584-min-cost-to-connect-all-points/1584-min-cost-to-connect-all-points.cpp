819e725cadc5e1935573ce94870499c625e334d0
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-06 09:46:24 +0900

    Time: 205 ms (84.58%), Space: 42.2 MB (77.39%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,69 +1,32 @@
-class UnionFind{
-  public:
-    vector<int> root;
-    vector<int> rank;
-    
-    UnionFind(int size){
-        this->root = vector<int>(size);
-        this->rank = vector<int>(size);
-        for(int i = 0; i < size; i++){
-            root[i] = i;
-            rank[i] = 0;
-        }
-    }
-    int find(int node){
-        if(node == root[node]){
-            return root[node];
-        }
-        return root[node] = find(root[node]);
-    }
-    bool join(int node1, int node2){
-        int group1 = find(node1);
-        int group2 = find(node2);
-        if(group1 == group2){
-            return false;
-        }
-        if(rank[group1] > rank[group2]){
-            root[group2] = group1;
-        }else if(rank[group1] < rank[group2]){
-            root[group1] = group2;
-        }else{
-            root[group1] = group2;
-            rank[group2]++;
-        }
-        return true;
-    }
-};
-
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int size = points.size();
-        int a = 0;
-        int x = 0;
-        int y = 0;
-        vector<pair<int,pair<int,int>>> fucking_graph;
-        for(int currNode = 0; currNode < size; currNode++){
-            for(int nextNode = currNode+1; nextNode < size; nextNode++){
-                a = abs(points[currNode][0] - points[nextNode][0]) + 
-                          abs(points[currNode][1] - points[nextNode][1]);
-                
-                fucking_graph.push_back({a,{currNode,nextNode}});
+        int n = points.size();
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> heap;
+        heap.push({0,0});
+        vector<bool> visited(n);
+        int mstCost = 0;
+        int edgesUsed = 0;
+        while(edgesUsed < n){
+            pair<int,int> topElement = heap.top();
+            heap.pop();
+            int weight = topElement.first;
+            int currNode = topElement.second;
+            
+            if(visited[currNode]){
+                continue;
+            }
+            visited[currNode] = true;
+            edgesUsed++;
+            mstCost+=weight;
+            for(int nextNode = 0; nextNode < n; nextNode++){
+                if(!visited[nextNode]){
+                    int nextweight = abs(points[currNode][0] - points[nextNode][0]) + abs(points[currNode][1] - points[nextNode][1]);
+                    heap.push({nextweight,nextNode});
+                }
+               
             }
         }
-        sort(fucking_graph.begin(),fucking_graph.end());
-        UnionFind uf(size);
-        int count = 0;
-        int result = 0;
-        for(int i = 0; i < fucking_graph.size() and count < size ;i++){
-            int node1 = fucking_graph[i].second.first;
-            int node2 = fucking_graph[i].second.second;
-            int weight = fucking_graph[i].first;
-            if(uf.join(node1,node2)){
-                result+=weight;
-                count++;
-            }
-        }
-        return result;
+        return mstCost;
     }
 };
