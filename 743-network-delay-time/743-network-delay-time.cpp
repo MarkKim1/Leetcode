b48b218135a3bc56931a4ddfc83b7b9f1a9dca4e
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-06 21:46:10 +0900

    Time: 307 ms (16.04%), Space: 43.7 MB (23.96%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,56 +1,37 @@
 class Solution {
 public:
-    // Adjacency list, defined it as per the maximum number of nodes
-    // But can be defined with the input size as well
-    
-    void BFS(vector<int>& signalReceivedAt, int k,vector<vector<pair<int, int>>>& adj) {
-        queue<int> q;
-        q.push(k);
-        
-        // Time for starting node is 0
-        signalReceivedAt[k] = 0;
-        
-        while (!q.empty()) {
-            int currNode = q.front(); 
-            q.pop();
-            
-            // Broadcast the signal to adjacent nodes
-            for (pair<int,int> edge : adj[currNode]) {
-                int time = edge.first;
-                int neighborNode = edge.second;
-                
-                int arrivalTime = signalReceivedAt[currNode] + time;
-                if (signalReceivedAt[neighborNode] > arrivalTime) {
-                    // Fastest signal time for neighborNode so far
-                    // signalReceivedAt[currNode] + time : 
-                    // time when signal reaches neighborNode
-                    signalReceivedAt[neighborNode] = arrivalTime;
-                    q.push(neighborNode);
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int,int>>> adj(n+1);
+        for(vector<int> time: times){
+            int from = time[0];
+            int to = time[1];
+            int distance = time[2];
+            adj[from].push_back({distance,to});
+        }
+        vector<int> signal_sum(n+1,INT_MAX);
+        int answer = INT_MIN;
+        BFS(adj,k,signal_sum);
+        signal_sum[k] = 0;
+        for(int i = 1; i <=n; i++){
+            answer = max(answer,signal_sum[i]);
+        }
+        return answer == INT_MAX ? -1: answer;
+    }
+    void BFS(vector<vector<pair<int,int>>>& adj, int k,vector<int>& signal_sum){
+        priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>>pq;
+        pq.push({0,k});//pq.first = distance, pq.second = node;
+        while(!pq.empty()){
+            int currnode = pq.top().second;
+            int weight = pq.top().first;
+            pq.pop();
+            for(auto neighborNode: adj[currnode]){
+                int newWeight = neighborNode.first;
+                int nextNode = neighborNode.second;
+                if(signal_sum[nextNode] > weight + newWeight){
+                    signal_sum[nextNode] = weight + newWeight;
+                    pq.push({weight + newWeight,nextNode});
                 }
             }
         }
-    }
-    
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        // Build the adjacency list
-        vector<vector<pair<int, int>>> adj(n+1);
-        for (vector<int> time : times) {
-            int source = time[0];
-            int dest = time[1];
-            int travelTime = time[2];
-            
-            adj[source].push_back({travelTime, dest});
-        }
-
-        vector<int> signalReceivedAt(n + 1, INT_MAX);
-        BFS(signalReceivedAt, k,adj);
-        
-        int answer = INT_MIN;
-        for (int i = 1; i <= n; i++) {
-            answer = max(answer, signalReceivedAt[i]);
-        }
-        
-        // INT_MAX signifies atleat one node is unreachable
-        return answer == INT_MAX ? -1 : answer;
     }
 };
