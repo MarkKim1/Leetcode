85cb94fc0d286d1ed7ceb186a46e5b03ea73f211
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-08 20:35:14 +0900

    Time: 22 ms (57.06%), Space: 10.9 MB (31.68%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>hash;
-        int numberToCount = 0;
+        unordered_map<int,int> map;
         for(int i = 0; i < nums.size(); i++){
-            numberToCount = target - nums[i];
-            
-            if(hash.find(numberToCount) != hash.end()){
-                return {hash[numberToCount], i};
+            int targetnum = target-nums[i];
+            if(map.find(targetnum) != map.end()){
+                return {i,map[targetnum]};
             }
-            hash[nums[i]] = i;
+            map[nums[i]] = i ;
         }
-        return{-1,-1};
+        return {0,0};
     }
 };
