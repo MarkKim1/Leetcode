da99c4d7ba1dc19d6bf66024f6cc58a82f13181f
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-09 19:06:55 +0900

    Time: 6 ms (45.51%), Space: 7.8 MB (55.27%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
+    
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> result;
-        //result.emplace_back(nums);
-        backtrack(nums,result,0);
+        backtrack(result,nums,0);
         return result;
     }
-    void backtrack(vector<int>& nums,vector<vector<int>>& result,int first){
-        if(first == nums.size()){
-            result.emplace_back(nums);
+    void backtrack(vector<vector<int>>& result,vector<int>& nums,int start){
+        if(start == nums.size()){
+            result.push_back(nums);
             return;
         }
-        for(int i = first; i < nums.size(); i++){
-            swap(nums[i],nums[first]);
-            backtrack(nums,result,first+1);
-            swap(nums[i],nums[first]);
+        for(int i = start; i < nums.size(); i++){
+            swap(nums[i],nums[start]);
+            backtrack(result,nums,start+1);
+            swap(nums[i],nums[start]);
         }
     }
 };
