d0d30784272003ddf9a11ac7d2161f0c0dfd76ac
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-14 17:10:05 +0900

    Time: 20 ms (31.00%), Space: 21 MB (25.83%) - LeetHub

--- 545-boundary-of-binary-tree/545-boundary-of-binary-tree.cpp
+++ 545-boundary-of-binary-tree/545-boundary-of-binary-tree.cpp
@@ -1,22 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> boundaryOfBinaryTree(TreeNode* root) {
-        vector<int> bounds;
-        if (root) {
-            bounds.push_back(root->val);
-            getBounds(root->left, bounds, true, false);
-            getBounds(root->right, bounds, false, true);
+        vector<int> result;
+        if(root){
+            result.push_back(root->val);
+            getBounds(root->left,result,true,false);
+            getBounds(root->right,result,false,true);
         }
-        return bounds;
+        return result;
     }
-
-private:
-    void getBounds(TreeNode* node, vector<int>& res, bool lb, bool rb) {
-        if (!node)  return;
-        if (lb) res.push_back(node->val);
-        if (!lb && !rb && !node->left && !node->right)  res.push_back(node->val);
-        getBounds(node->left, res, lb, rb && node->right == NULL);
-        getBounds(node->right, res, lb && node->left == NULL, rb);
-        if (rb) res.push_back(node->val);
+    void getBounds(TreeNode* node, vector<int>& result, bool lb, bool rb){
+        if(node == NULL) return;
+        if(lb) result.push_back(node->val);
+        if(!lb and !rb and node->left == NULL and node->right == NULL) result.push_back(node->val);
+        getBounds(node->left,result,lb,rb and node->right == NULL);
+        getBounds(node->right,result,lb and node->left == NULL,rb);
+        if(rb) result.push_back(node->val);
     }
 };
