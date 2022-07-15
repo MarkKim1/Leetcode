33352f34fa0fcb2d0a4f17bbd73c74fa1b36f2a1
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-15 20:50:10 +0900

    Time: 31 ms (61.06%), Space: 26.3 MB (58.14%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,30 +11,21 @@
  */
 class Solution {
 public:
-    unordered_map<int,int> map;
+    unordered_map<int,int>map;
     int preorderIndex;
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        for(int i = 0; i < inorder.size();i++){
+        for(int i = 0; i < inorder.size(); i++){
             map[inorder[i]] = i;
         }
+        preorderIndex = 0;
         return build(preorder,0,preorder.size()-1);
     }
-    TreeNode* build(vector<int>& preorder,int left, int right){
+    TreeNode* build(vector<int>& preorder, int left, int right){
         if(left > right) return NULL;
-        
         int rootValue = preorder[preorderIndex++];
-        TreeNode *root = new TreeNode(rootValue);
+        TreeNode* root = new TreeNode(rootValue);
         root->left = build(preorder,left,map[rootValue]-1);
-        root->right = build(preorder,map[rootValue] + 1,right);
+        root->right = build(preorder,map[rootValue]+1,right);
         return root;
-        // if(curr < preorder.size() and curr >= 0 and pivot >= 0 and pivot < preorder.size()){
-        //     root = new TreeNode(inorder[map[preorder[curr]]]);
-        //     pivot = map[preorder[curr]];
-        //     curr++;
-        //     build(root->left,inorder,preorder,pivot-1,seen);
-        //     build(root->right,inorder,preorder,pivot+1,seen);
-        // }else{
-        //     return;
-        // }
     }
 };
