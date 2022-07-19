1f4113cc9a6f643f603b73870bef93d9615dc27f
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-19 15:33:25 +0900

    Time: 37 ms (26.12%), Space: 13.3 MB (36.98%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        set<string> word_set(wordDict.begin(), wordDict.end());
-        // In the memo table, -1 stands for the state not yet reached,
-        // 0 for false and 1 for true
-        vector<int> memo(s.length(), -1);
-        return wordBreakMemo(s, word_set, 0, memo);
-    }
-
-    bool wordBreakMemo(string& s, set<string>& word_set, int start, vector<int>& memo) {
-        if (start == s.length()) {
-            return true;
-        }
-        if (memo[start] != -1) {
-            return memo[start];
-        }
-        for (int end = start + 1; end <= s.length(); end++) {
-            if (word_set.find(s.substr(start, end - start)) != word_set.end() and
-                wordBreakMemo(s, word_set, end, memo)) {
-                return memo[start] = true;
+        set<string> word(wordDict.begin(),wordDict.end());
+        // cout << s.substr(0,4) << endl;
+        // cout << s.substr(4,8) << endl;
+        vector<bool> dp(s.size()+1);
+        dp[0] = true;
+        for(int i = 1; i <= s.size(); i++){
+            for(int j = 0; j < i; j++){
+                if(dp[j] and word.find(s.substr(j,i-j)) != word.end()){
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        return memo[start] = false;
+        return dp[s.size()];
+        //   0 1 2 3 4 5 6 7 8    
+        //dp T F F F T F F F T
+        
+        //0 1 2 3 4 5 6 7
+        //l e e t c o d e
     }
 };
