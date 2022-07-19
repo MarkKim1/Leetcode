967de4719764a1fd654ad51746773f5ce4ec73bd
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-19 17:10:46 +0900

    Time: 5 ms (43.01%), Space: 6.2 MB (8.59%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n <= 1){
-            return n;
+        if(n == 0) return 0;
+        vector<int> F(n+1);
+        F[0] = 0;
+        F[1] = 1;
+        for(int i = 2; i <= n; i++){
+            F[i] = F[i-1] + F[i-2];
         }
-        return fib(n-1) + fib(n-2);
+        return F[n];
     }
 };
