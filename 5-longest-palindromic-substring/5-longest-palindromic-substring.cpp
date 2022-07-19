d196eddc71ec1d86ab2da80f0a515c7b11611f8c
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-19 21:18:26 +0900

    Time: 1481 ms (5.34%), Space: 37 MB (28.28%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
-        vector<vector<bool>> p(n,vector<bool>(n,false));
-        for(int i = 0; i < n; i++){
-            p[i][i] = true;
-            if(i == n-1) break;
-            p[i][i+1] = (s[i] == s[i+1]);
+        vector<vector<bool>> dp(s.size(),vector<bool>(s.size()));
+        if(s.size() == 1) return s;
+        for(int i = 0; i < s.size(); i++){
+            dp[i][i] = true;
+            if(i == s.size()-1)break;
+            dp[i][i+1] = s[i] == s[i+1];
         }
-        
-        for(int i = n-3; i>=0; --i){
-            for(int j = i+2; j < n; j++){
-                p[i][j] = (p[i+1][j-1] and s[i] == s[j]);
+        for(int i = s.size()-3; i>=0; --i){
+            for(int j = i+2; j < s.size(); j++){
+               dp[i][j] = (dp[i+1][j-1] and s[i] == s[j]);
             }
         }
-        
-        int max = 0;
-        string maxstr = "";
-        
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(p[i][j] == true and j-i+1>max){
-                    max = j-i+1;
-                    maxstr = s.substr(i,j-i+1);
+        int distance = INT_MIN;
+        string result = "";
+        for(int i = 0; i < s.size(); i++){
+            for(int j = i; j < s.size(); j++){
+                if(dp[i][j] == true and j-i+1 > distance){
+                    distance = j-i+1;
+                    result = s.substr(i,distance);
                 }
             }
         }
-        return maxstr;
+        return result;
     }
 };
