f1715ca8fa3b39780b0e01b997a863e688b49d9d
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-19 16:44:26 +0900

    Time: 1929 ms (5.02%), Space: 274.5 MB (5.05%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        set<int> dp;
+        dp.insert(0);
+        int totalSum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            totalSum+=nums[i];
+        }
+        if(totalSum % 2 != 0)return false;
+        int target = totalSum/2;
+        for(int i = nums.size()-1; i >= 0; i--){
+            set<int> nextdp;
+            nextdp.insert(nums[i]);
+            for(auto t: dp){
+                if(t+nums[i] == target) return true;
+                nextdp.insert(t+nums[i]);
+                nextdp.insert(t);
+            }
+            dp = nextdp;
+        }
+        return false;
+    }
+};
