fb5fed836e8edfe24d1c564d42f838ad564dc964
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-19 17:16:18 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (19.19%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
-    
     int tribonacci(int n) {
-        unordered_map<int,int> memo;
-        memo[0] = 0;
-        memo[1] = 1;
-        memo[2] = 1;
-        return dp(n, memo);
-        
-    }
-    
-    int dp(int n, unordered_map<int,int>& memo){
-        if(memo.find(n) != memo.end()) return memo[n];
         if(n == 0) return 0;
-        memo[n] = dp(n-1,memo) + dp(n-2,memo) + dp(n-3,memo);
-        return memo[n];
+        if(n < 3) return 1;
+        vector<int> T(n+1);
+        T[0] = 0;
+        T[1] = 1;
+        T[2] = 1;
+        for(int i = 3; i <= n; i++){
+            T[i] = T[i-1] + T[i-2] + T[i-3];
+        }
+        return T[n];
     }
 };
