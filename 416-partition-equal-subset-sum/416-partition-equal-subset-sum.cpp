2950d9a28490a2c018e7bb116cf53ef2c249debd
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-19 16:49:23 +0900

    Time: 1398 ms (5.02%), Space: 248.7 MB (5.05%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    bool canPartition(vector<int> &nums) {
+    bool canPartition(vector<int>& nums) {
+        unordered_set<int> dp;
+        dp.insert(0);
         int totalSum = 0;
-        // find sum of all array elements
-        for (int num : nums) {
-            totalSum += num;
+        for(int i = 0; i < nums.size(); i++){
+            totalSum+=nums[i];
         }
-        // if totalSum is odd, it cannot be partitioned into equal sum subset
-        if (totalSum % 2 != 0) return false;
-        int subSetSum = totalSum / 2;
-        int n = nums.size();
-        vector<bool> dp(subSetSum + 1, false);
-        dp[0] = true;
-        for (int curr : nums) {
-            for (int j = subSetSum; j >= curr; j--) {
-                dp[j] = dp[j] || dp[j - curr];
+        if(totalSum % 2 != 0)return false;
+        int target = totalSum/2;
+        for(int i = nums.size()-1; i >= 0; i--){
+            unordered_set<int> nextdp;
+            nextdp.insert(nums[i]);
+            for(auto t: dp){
+                if(t+nums[i] == target) return true;
+                nextdp.insert(t+nums[i]);
+                nextdp.insert(t);
             }
+            dp = nextdp;
         }
-        return dp[subSetSum];
+        return false;
     }
 };
