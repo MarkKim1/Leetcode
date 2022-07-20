6741b9f8c34cdf8972e687e6edc05351f948cd3a
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-20 19:19:54 +0900

    Time: 162 ms (44.61%), Space: 17.3 MB (49.66%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -6,20 +6,22 @@ public:
         sort(words.begin(),words.end(),[](const string& word,const string& word1){
                 return (word.size() < word1.size());
             });;
-        int longestWordSequenceLength = 1;
-        for(auto &word: words){
-            int presentLength = 1;
+        int longest = 1;
+        for(auto word: words){
+            int presentlength = 1;
             for(int i = 0; i < word.size(); i++){
-                string pred = word.substr(0,i) + word.substr(i+1,word.size()+1);
+                string first = word.substr(0,i);
+                string second = word.substr(i+1,word.size()+1);
+                string pred = first + second;
                 if(dp.find(pred) != dp.end()){
-                    int prevLength = dp[pred];
-                    presentLength = max(presentLength,prevLength+1);
+                    int length = dp[pred];
+                    presentlength = max(presentlength,length+1);
                 }
+                dp[word] = presentlength;
+                longest = max(longest,presentlength);
             }
-            dp[word] = presentLength;
-            longestWordSequenceLength = max(longestWordSequenceLength,presentLength);
         }
-        return longestWordSequenceLength;
+        return longest;
     }
     
 };
