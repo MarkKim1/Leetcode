5d1ac0f2577649596944c9b7f680427b0b484ff1
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-22 02:32:44 +0900

    Time: 4 ms (35.28%), Space: 7.8 MB (24.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -8,6 +8,6 @@ public:
         for(int i = 2; i < nums.size(); i++){
             dp[i] = max(dp[i-1],dp[i-2] + nums[i]);
         }
-        return dp[nums.size()-1];
+        return dp.back();
     }
 };
