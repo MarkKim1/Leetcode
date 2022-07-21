b49c7cedf2e7e2080e990f350064a85722992bad
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-21 17:34:08 +0900

    Time: 82 ms (86.98%), Space: 17.2 MB (64.90%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,27 +1,24 @@
-
 class Solution {
 public:
     int longestStrChain(vector<string>& words) {
+        sort(words.begin(),words.end(),[](const string& word1, const string& word2){
+            return word1.size() < word2.size();
+        });
         unordered_map<string,int> dp;
-        sort(words.begin(),words.end(),[](const string& word,const string& word1){
-                return (word.size() < word1.size());
-            });;
         int longest = 1;
-        for(auto word: words){
-            int presentlength = 1;
-            for(int i = 0; i < word.size(); i++){
-                string first = word.substr(0,i);
-                string second = word.substr(i+1,word.size()+1);
-                string pred = first + second;
-                if(dp.find(pred) != dp.end()){
-                    int length = dp[pred];
-                    presentlength = max(presentlength,length+1);
+        for(int i = 0; i < words.size(); i++){
+            int currLongestCount = 1;
+            string currString = words[i];
+            for(int j = 0; j < currString.size(); j++){
+                string stringToCompare = currString.substr(0,j) + currString.substr(j+1,currString.size()+1);
+                if(dp.find(stringToCompare) != dp.end()){
+                    int dpVal = dp[stringToCompare];
+                    currLongestCount = max(currLongestCount,dpVal+1);
                 }
-                dp[word] = presentlength;
-                longest = max(longest,presentlength);
+                dp[currString] = currLongestCount;
+                longest = max(longest,currLongestCount);
             }
         }
         return longest;
     }
-    
 };
