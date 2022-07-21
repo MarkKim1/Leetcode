fb8f3d9a8fbb8b15e7ea6561a865ae32d7d1aac3
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-21 17:37:45 +0900

    Time: 3 ms (96.54%), Space: 5.9 MB (94.02%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    double champagneTower(int poured, int query_row, int query_glass) {
-        double result[102][102] = {0,0};
-        result[0][0] = (double)poured;
-        for(int i = 0; i <= query_row; i++){
-            for(int j = 0; j <= query_glass; j++){
-                double amountForNextGlass = (result[i][j] - 1.00) / 2.00;
-                if(amountForNextGlass > 0){
-                    result[i+1][j] += amountForNextGlass;
-                    result[i+1][j+1] += amountForNextGlass;
+    double champagneTower(int poured, int row, int col) {
+        double dp[102][102] = {0.0};
+        dp[0][0] = (double)poured;
+        for(int i = 0; i <= row; i++){
+            for(int j = 0; j <= col; j++){
+                double toPour = (dp[i][j] - 1.0) / 2.0;
+                if(toPour > 0){
+                    dp[i+1][j] += toPour;
+                    dp[i+1][j+1] += toPour;
                 }
             }
         }
-        double answer = min(1.000000,result[query_row][query_glass]);
-        return (double)answer;
+        double answer = min(1.0, dp[row][col]);
+        return answer;
     }
 };
