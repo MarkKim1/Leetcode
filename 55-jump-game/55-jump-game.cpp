fe26b42a3d69acef1a96c90ba2c4f66938b92bc6
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-21 20:02:04 +0900

    Time: 127 ms (35.81%), Space: 48.4 MB (43.19%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int mx = 0;
-        for(int i = 0; i < nums.size(); i++)
-            if(mx >= i)
-                mx = max(mx, i + nums[i]);
-        return (mx >= nums.size()-1);
+        int reach = 0;
+        for(int i = 0; i <= reach; i++){
+            reach = max(reach,i+nums[i]);
+            if(reach >= nums.size()-1){
+                return true;
+            }
+        }
+        return false;
     }
 };
