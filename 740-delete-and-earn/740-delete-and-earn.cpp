20ee2a046b1f33ea9562e8e86c1817eb8620be2a
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-25 04:25:20 +0900

    Time: 15 ms (81.65%), Space: 12.6 MB (75.91%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,28 +1,56 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        if(nums.size() == 1) return nums[0];
+        if(nums.size() == 0) return 0;
+        if(nums.size() == 1) return nums[0]; 
         sort(nums.begin(),nums.end());
         unordered_map<int,int> map;
-        int one = 0,two = 0;
         for(int i = 0; i < nums.size(); i++){
             map[nums[i]]+=nums[i];
         }
-        
         nums.erase(unique(nums.begin(),nums.end()),nums.end());
-        
+        int one = 0;
+        int two = 0;
         for(int i = 0; i < nums.size(); i++){
-            if(i > 0 and nums[i] == nums[i-1]+1){
-                int temp = two;
-                two = max(map[nums[i]]+one,two);
-                one = temp;
+            if(i > 0 and nums[i-1]+1 == nums[i]){
+                int temp = one;
+                one = max(two + map[nums[i]],one);
+                two = temp;
             }else{
-                int temp = two;
-                two = map[nums[i]] + two;
-                one = temp;
+                int temp = one;
+                one = one + map[nums[i]];
+                two = temp;
             }
         }
-        
-        return two;
+        return one;
     }
 };
+
+// class Solution {
+// public:
+//     int deleteAndEarn(vector<int>& nums) {
+//         if(nums.size() == 1) return nums[0];
+//         sort(nums.begin(),nums.end());
+//         unordered_map<int,int> map;
+//         int one = 0,two = 0;
+//         for(int i = 0; i < nums.size(); i++){
+//             map[nums[i]]+=nums[i];
+//         }
+        
+//         nums.erase(unique(nums.begin(),nums.end()),nums.end());
+        
+//         for(int i = 0; i < nums.size(); i++){
+//             if(i > 0 and nums[i] == nums[i-1]+1){
+//                 int temp = two;
+//                 two = max(map[nums[i]]+one,two);
+//                 one = temp;
+//             }else{
+//                 int temp = two;
+//                 two = map[nums[i]] + two;
+//                 one = temp;
+//             }
+//         }
+        
+//         return two;
+//     }
+// };
