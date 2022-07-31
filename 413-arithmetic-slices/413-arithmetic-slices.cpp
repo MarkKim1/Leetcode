5e3d85ca8d4e447060b64bba3b3d6fadfe3d7b3d
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-07-31 17:49:32 +0900

    Time: 7 ms (20.03%), Space: 7.4 MB (13.37%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    int sum = 0;
     int numberOfArithmeticSlices(vector<int>& nums) {
-        slices(nums,nums.size()-1);
-        return sum;
-    }
-    int slices(vector<int>& nums, int i){
-        if(i < 2){
-            return 0;
+        if(nums.size() < 3) return 0; 
+        int result = 0;
+        vector<int> dp(nums.size(),0);
+        for(int i = 2; i < nums.size(); i++){
+            if(nums[i]-nums[i-1] == nums[i-1] - nums[i-2]){
+                dp[i] = dp[i-1] + 1;
+                result+=dp[i];
+            }
         }
-        int ap = 0;
-        if(nums[i]-nums[i-1] == nums[i-1] - nums[i-2]){
-            ap = 1 + slices(nums,i-1);
-            sum +=ap;
-        }else{
-            slices(nums,i-1);
-        }
-        return ap;
+        return result;
     }
 };
