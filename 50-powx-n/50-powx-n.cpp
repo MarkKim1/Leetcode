34d1b639d732a3a5e27d268ae47afa743b8aa5ee
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-08-01 02:53:56 +0900

    Time: 0 ms (100.00%), Space: 6.3 MB (12.99%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        int result = x;
+        for(int i = 0; i < n; i++){
+           
+        }
+        return pow(x,n);
+    }
+};
