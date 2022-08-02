f1f641dd4a455db57157f9dd5fc3bde124ef0fdf
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-08-02 21:11:02 +0900

    Time: 3 ms (39.24%), Space: 6.6 MB (32.60%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
+        if(numRows == 1) return{{1}};
         vector<vector<int>> result;
-        vector<int> sub_result;
+        vector<int> current;
         result.push_back({1});
-        for(int rowNum = 1; rowNum < numRows; rowNum++){
-            vector<int> prev_row = result[rowNum-1];
-            sub_result.push_back(1);
-            for(int col = 1; col < prev_row.size(); col++){
-                sub_result.push_back(prev_row[col-1] + prev_row[col]);
+        result.push_back({1,1});
+        for(int i = 2; i <numRows; i++){
+            for(int j = 0; j <= i; j++){
+                if(j == 0){
+                    current.push_back(1);
+                    continue;
+                }
+                if(j == i){
+                    current.push_back(1);
+                    continue;
+                }
+                current.push_back(result[i-1][j] + result[i-1][j-1]);
             }
-            sub_result.push_back(1);
-            result.push_back(sub_result);
-            sub_result.clear();
+            result.push_back(current);
+            current.clear();
         }
         return result;
     }
