0a680bcae40be42f9e80519cba7e96a809b92e19
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-08-08 19:20:38 +0900

    Time: 123 ms (34.33%), Space: 18 MB (72.81%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        vector<vector<int>> dp(matrix.size(),vector<int>(matrix[0].size(),0));
+        vector<vector<int>> dp(matrix.size(),vector<int>(matrix[0].size()));
+        int ROW = matrix.size();
+        int COL = matrix[0].size();
         int area = 0;
-        for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[0].size(); j++){
-                if(i == 0 || j == 0){
-                    dp[i][j] = matrix[i][j] == '1' ? 1: 0;
+        for(int i = 0; i < ROW; i++){
+            for(int j = 0; j < COL; j++){
+                if(i == 0 or j == 0){
+                    dp[i][j] = matrix[i][j] == '1' ? 1 : 0;
                     area = max(area,dp[i][j]);
                 }else if(matrix[i][j] == '1'){
-                    dp[i][j] = min(dp[i][j-1],min(dp[i-1][j-1],dp[i-1][j])) + 1;
+                    dp[i][j] = min(min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1]) + 1;
                     area = max(area,dp[i][j]);
                 }
             }
         }
-        return area*area;
+        return area * area;
     }
 };
