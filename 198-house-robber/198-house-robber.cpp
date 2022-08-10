1508e8e5dc0812177e6d0c6490e3b80e0c6fd210
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-08-11 02:04:03 +0900

    Time: 0 ms (100.00%), Space: 7.9 MB (25.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -4,10 +4,14 @@ public:
         if(nums.size() == 1) return nums[0];
         vector<int> dp(nums.size(),0);
         dp[0] = nums[0];
-        dp[1] = max(nums[0],nums[1]);
-        for(int i = 2; i < nums.size(); i++){
-            dp[i] = max(dp[i-1],dp[i-2] + nums[i]);
+        dp[1] = nums[1];
+        if(nums.size() == 2){
+            return max(dp[0],dp[1]);
         }
-        return dp.back();
+        dp[2] = dp[0] + nums[2];
+        for(int i = 3; i < nums.size(); i++){
+            dp[i] = max(dp[i-2],dp[i-3]) + nums[i];
+        }
+        return max(dp[nums.size()-1],dp[nums.size()-2]);
     }
 };
