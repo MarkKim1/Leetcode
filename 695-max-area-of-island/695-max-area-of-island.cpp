e08fc28206bc2f3f75fdbf3c5a5744e130e45e26
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-08-13 20:47:56 +0900

    Time: 23 ms (78.10%), Space: 23.2 MB (61.64%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,39 +1,29 @@
 class Solution {
-    public:
-    int DFS(vector<vector<int>>&grid,int n, int m,int y, int x,int count){
-        if(n <= -1 || n >= y || m <= -1 || m >= x){
-            return count;
-        }
-        else if(grid[n][m] == 1){
-            grid[n][m] = 0;
-            count ++;
-            count =DFS(grid,n-1,m,y,x,count);
-            count =DFS(grid,n+1,m,y,x,count);
-            count =DFS(grid,n,m-1,y,x,count);
-            count =DFS(grid,n,m+1,y,x,count);
-            
-        }
-        return count;
-    }
-    
+public:
+    int result = 0;
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        const int y = grid.size();
-        const int x = grid[0].size();
-        int n = y;
-        int m = x;
-        int max = 0;
+        int ROW = grid.size();
+        int COL = grid[0].size();
         int count = 0;
-        int result = 0;
-        for(int i = 0; i < y; i++){
-            for(int j = 0; j < x; j++){
+        for(int i = 0; i < ROW; i++){
+            for(int j = 0; j < COL; j++){
                 if(grid[i][j] == 1){
-                    result = DFS(grid,i,j,y,x,count);
-                    if(max <= result){
-                        max = result;
-                    }
+                    dfs(grid,i,j);
+                    count = max(count,result);
+                    result = 0;
                 }
             }
         }
-        return max;
+        return count;
+    }
+    void dfs(vector<vector<int>>& grid, int i, int j){
+        if(i >= 0 and j >= 0 and i < grid.size() and j < grid[0].size() and grid[i][j] == 1){
+            grid[i][j] = 0;
+            result++;
+            dfs(grid,i+1,j);
+            dfs(grid,i-1,j);
+            dfs(grid,i,j+1);
+            dfs(grid,i,j-1);
+        }
     }
 };
