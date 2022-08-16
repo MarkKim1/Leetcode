471c1110f3d05186172ad0fcbe9b61aa075cbdea
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-08-17 02:38:59 +0900

    Time: 119 ms (56.73%), Space: 30.4 MB (38.42%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        if (rows == 0)
-            return matrix;
-        int cols = matrix[0].size();
-        vector<vector<int>> dist(rows, vector<int> (cols, INT_MAX));
-        queue<pair<int, int>> q;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (matrix[i][j] == 0) {
-                    dist[i][j] = 0;
-                    q.push({ i, j }); //Put all 0s in the queue.
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        vector<vector<int>> result(mat.size(),vector<int>(mat[0].size(),INT_MAX));
+        queue<pair<int,int>> q;
+        int row = mat.size();
+        int col = mat[0].size();
+        
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(mat[i][j] == 0){
+                    result[i][j] = 0;
+                    q.push({i,j});
                 }
             }
         }
-
-        int dir[4][2] = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-        while (!q.empty()) {
-            pair<int, int> curr = q.front();
+        vector<int> x = {0,1,0,-1};
+        vector<int> y = {1,0,-1,0};
+        while(!q.empty()){
+            auto it = q.front();
             q.pop();
-            for (int i = 0; i < 4; i++) {
-                int new_r = curr.first + dir[i][0], new_c = curr.second + dir[i][1];
-                if (new_r >= 0 && new_c >= 0 && new_r < rows && new_c < cols) {
-                    if (dist[new_r][new_c] > dist[curr.first][curr.second] + 1) {
-                        dist[new_r][new_c] = dist[curr.first][curr.second] + 1;
-                        q.push({ new_r, new_c });
+            for(int i = 0; i < 4; i++){
+                int new_row = it.first + x[i];
+                int new_col = it.second + y[i];
+                if(new_row >= 0 and new_col >= 0 and new_row < row and new_col < col){
+                    if(result[new_row][new_col] > result[it.first][it.second]+1){
+                        result[new_row][new_col] = result[it.first][it.second] + 1;
+                        q.push(make_pair(new_row,new_col));
                     }
                 }
             }
         }
-        return dist;
+        return result;
     }
 };
