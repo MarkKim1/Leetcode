c6afed4ef81d42995bc24ff977a2a27db32e68da
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-08-20 16:04:32 +0900

    Time: 38 ms (30.39%), Space: 19.8 MB (9.83%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        vector<int> sub;
-        vector<vector<int>> result;
-        dfs(sub,result,0,graph);
-        return result;
-    }
-    void dfs(vector<int>& sub, vector<vector<int>>& result, int curr,vector<vector<int>>& graph){
-        if(curr == graph.size()-1){
-            sub.push_back(graph.size()-1);
-            result.push_back(sub);
-            return;
-        }
-        sub.push_back(curr);
-        for(int i = 0; i < graph[curr].size(); i++){
-            
-            dfs(sub,result,graph[curr][i],graph);
-            sub.pop_back();
-        }
+        int target = int(graph.size()) - 1;
+        map<int, vector<vector<int>>> memo;
+        function<vector<vector<int>>(int)> allPathsToTarget = [&](int currNode) {
+            if (memo.count(currNode)) return memo[currNode];
+            vector<vector<int>> results;
+            if (currNode == target) {
+                results.push_back(vector<int>{target});
+            } else {
+                for (int nextNode : graph[currNode]) {
+                    for (vector<int>& path : allPathsToTarget(nextNode)) {
+                        vector<int> newPath{currNode};
+                        newPath.insert(newPath.end(), path.begin(), path.end());
+                        results.push_back(newPath);
+                    }
+                }
+            }
+            memo[currNode] = results;
+            return results;
+        };
+        return allPathsToTarget(0);
     }
 };
