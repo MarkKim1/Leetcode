da06b2450500559f966cbd921a7eb351573ecb02
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-08-20 23:08:15 +0900

    Time: 320 ms (86.26%), Space: 122.2 MB (58.15%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int dfs(int i, vector<vector<int>> &m, vector<int>& informTime, int mx = 0) {
+    for (auto d : m[i])
+        mx = max(mx, dfs(d, m, informTime));
+    return informTime[i] + mx;
+}
+int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+    vector<vector<int>> m(n);
+    for(auto i = 0; i < manager.size(); ++i)
+        if (manager[i] != -1)
+            m[manager[i]].push_back(i);
+    return dfs(headID, m, informTime);
+}
+};
