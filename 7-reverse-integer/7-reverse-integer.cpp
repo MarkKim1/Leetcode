47355e28e02885fc6df6b87a0ec2741bf8b8719c
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-08-23 21:48:24 +0900

    Time: 0 ms (100.00%), Space: 5.7 MB (99.97%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -5,13 +5,9 @@ public:
         int ans = 0;
         while(temp != 0){
             int num = temp%10;
-            if(num == 0){
-                ans = ans*10;
-            }else{
-                if(ans > INT_MAX/10 || (ans == INT_MAX/10 and num > 7)) return 0;
-                if(ans < INT_MIN/10 || (ans == INT_MIN/10 and num < -8)) return 0;
-                ans = ans*10 + num;
-            }
+            if(ans > INT_MAX/10 || (ans == INT_MAX/10 and num > 7)) return 0;
+            if(ans < INT_MIN/10 || (ans == INT_MIN/10 and num < -8)) return 0;
+            ans = ans*10 + num;
             temp /=10;
         }
         return x < 0 ? -ans : ans;
