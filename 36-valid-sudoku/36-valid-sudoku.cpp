82de4da5b62a4600b612a06f8776f16704d73070
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-08-26 00:50:20 +0900

    Time: 24 ms (86.96%), Space: 18.1 MB (73.85%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<int,vector<int>> rows;
-        unordered_map<int,vector<int>> cols;
-        unordered_map<int,vector<int>> box;
+        int rows[9][9] = {0};
+        int  cols[9][9] = {0};
+        int  box[9][9] = {0};
         
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
                 if(board[i][j] == '.'){
                     continue;
                 }
-                if(find(rows[i].begin(),rows[i].end(),board[i][j]-'0') != rows[i].end()){
+                int num = board[i][j] - '0'-1;
+                int k = i / 3 * 3 + j/3;
+                if(rows[i][num] || cols[j][num] || box[k][num]){
                     return false;
                 }
-                rows[i].push_back(board[i][j]-'0');
-                if(find(cols[j].begin(),cols[j].end(),board[i][j]-'0') != cols[j].end()){
-                    return false;
-                }
-                cols[j].push_back(board[i][j]-'0');
-                int k = (i / 3) * 3 + j/3;
-                if(find(box[k].begin(),box[k].end(),board[i][j]-'0') != box[k].end()){
-                    return false;
-                }
-                box[k].push_back(board[i][j]-'0');
+                rows[i][num] = cols[j][num] = box[k][num] = 1;
             }
         }
         return true;
