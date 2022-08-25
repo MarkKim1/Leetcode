20b83d1b07c790e0c2ac910915868e16e2d4528f
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-08-25 22:11:48 +0900

    Time: 11 ms (73.16%), Space: 11.4 MB (43.23%) - LeetHub

--- 179-largest-number/179-largest-number.cpp
+++ 179-largest-number/179-largest-number.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     string largestNumber(vector<int>& nums) {
         vector<string> str;
-        long long count = 0;
         for(auto a : nums){
-            count+=a;
             str.push_back(to_string(a));
         }
-        if(count == 0) return "0";
         sort(str.begin(),str.end(),[](string s1, string s2){return s1+s2 > s2+s1;});
         string result = "";
         for(int i = 0; i < str.size(); i++){
+            if(result[0] == '0'){
+                continue;
+            }
             result+=str[i];
         }
         return result;
