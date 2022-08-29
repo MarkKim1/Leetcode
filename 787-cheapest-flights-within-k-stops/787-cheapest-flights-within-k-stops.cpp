fc3b9b24d2eb5d7a2c20b112d89f6ebd1e7db5d8
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-08-30 01:40:24 +0900

    Time: 245 ms (22.03%), Space: 49.7 MB (10.24%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -3,35 +3,18 @@ public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
         vector<int> prices(n,INT_MAX);
         prices[src] = 0;
-        for(int i = 0; i <=k; i++){
+        for(int i = 0; i <= k; i++){
             vector<int> temp(prices);
             for(auto f : flights){
-                if(prices[f[0]]==INT_MAX){
+                if(prices[f[0]] == INT_MAX){
                     continue;
                 }
-                temp[f[1]] = min(temp[f[1]],prices[f[0]] + f[2]);
+                if(prices[f[0]] + f[2] < temp[f[1]]){
+                    temp[f[1]] = prices[f[0]] + f[2];
+                }
             }
             prices = temp;
         }
         return prices[dst] == INT_MAX ? -1 : prices[dst];
     }
 };
-
-// class Solution {
-// public:
-//     //bellman ford.
-//     //just run it k+1 iterations.
-//     int findCheapestPrice(int n, vector<vector<int>>& a, int src, int sink, int k) {
-        
-//         vector<int> c(n, 1e8);
-//         c[src] = 0;
-        
-//         for(int z=0; z<=k; z++){
-//             vector<int> C(c);
-//             for(auto e: a)
-//                 C[e[1]] = min(C[e[1]], c[e[0]] + e[2]);
-//             c = C;
-//         }
-//         return c[sink] == 1e8 ? -1 : c[sink];
-//     }
-// };
