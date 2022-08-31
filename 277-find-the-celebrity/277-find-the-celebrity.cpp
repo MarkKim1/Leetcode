9fcd31d41a15fdd3ca28fcbffac9068bef69769f
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-08-31 11:54:30 +0900

    Time: 177 ms (98.58%), Space: 9.7 MB (81.42%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.cpp
+++ 277-find-the-celebrity/277-find-the-celebrity.cpp
@@ -1,24 +1,27 @@
-public class Solution extends Relation {
-    
-    private int numberOfPeople;
-    
-    public int findCelebrity(int n) {
-        numberOfPeople = n;
-        for (int i = 0; i < n; i++) {
-            if (isCelebrity(i)) {
-                return i;
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
+class Solution {
+public:
+    int findCelebrity(int n) {
+        int start = 0;
+        for(int i = 1; i < n; i++){
+            if(knows(start,i)){
+                start = i;
             }
+        }
+        if(isCelebrity(start,n)){
+            return start;
         }
         return -1;
     }
-    
-    private boolean isCelebrity(int i) {
-        for (int j = 0; j < numberOfPeople; j++) {
-            if (i == j) continue; // Don't ask if they know themselves.
-            if (knows(i, j) || !knows(j, i)) {
+    bool isCelebrity(int i, int n){
+        for(int j = 0; j < n; j++){
+            if(i == j) continue;
+            if(knows(i,j) || !knows(j,i)){
                 return false;
             }
         }
         return true;
     }
-}
+};
