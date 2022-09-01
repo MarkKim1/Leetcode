aa221c6d31064c87376e2a094b64877a4661caea
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-01 13:14:01 +0900

    Time: 240 ms (18.01%), Space: 39.1 MB (35.53%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-       vector<set<int>> graph(n);
+       vector<unordered_set<int>> graph(n);
         for(int i = 0; i < roads.size(); i++){
             graph[roads[i][0]].insert(roads[i][1]);
             graph[roads[i][1]].insert(roads[i][0]);
