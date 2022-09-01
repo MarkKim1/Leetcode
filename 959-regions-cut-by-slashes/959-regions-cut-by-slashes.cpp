d4d0a7195ccdcfcef5560ba2c34869dc23aee9fb
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-01 12:22:09 +0900

    Time: 47 ms (14.87%), Space: 11.4 MB (41.57%) - LeetHub

--- 959-regions-cut-by-slashes/959-regions-cut-by-slashes.cpp
+++ 959-regions-cut-by-slashes/959-regions-cut-by-slashes.cpp
@@ -1,56 +1,31 @@
-class DSU{
-    public:
-    vector<int> parents;
-    DSU(int x){
-        for(int i = 0; i < x; i++){
-            parents.push_back(i);
-        }
-    }
-    int find(int x){
-        if(x == parents[x]){
-            return x;
-        }
-        return find(parents[x]);
-    }
-    void uf(int a, int b){
-        int rootx = find(a);
-        int rooty = find(b);
-        if(rootx != rooty){
-            parents[rootx] = rooty;
-        }
-    }
-};
-
 class Solution {
 public:
     int regionsBySlashes(vector<string>& grid) {
         int n = grid.size();
-        DSU *dsu = new DSU(4*n*n);
+        vector<vector<int>> g(3*n,vector<int>(3*n));
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                int root = 4*(i*n+j);
-                char val = grid[i][j];
-                if(val != '\\'){       
-                    dsu->uf(root + 0, root + 1);
-                    dsu->uf(root + 2, root + 3);
+                if(grid[i][j] == '/'){
+                    g[3*i][3*j+2] = g[3*i+1][3*j+1] = g[3*i+2][3*j] = 1;
                 }
-                if(val != '/'){
-                    dsu->uf(root + 0, root + 2);
-                    dsu->uf(root + 1, root + 3);
-                }
-                if(i + 1 < n){
-                    dsu->uf(root + 3,(root + 4 * n) + 0);
-                }
-                if(j + 1 < n){
-                    dsu->uf(root + 2, (root + 4) + 1);
+                if(grid[i][j] == '\\'){
+                    g[3*i][3*j] = g[3*i+1][3*j+1] = g[3*i+2][3*j+2] = 1;
                 }
             }
         }
         int ans = 0;
-        for(int i = 0; i < 4 * n * n; i++){
-            if(i == dsu->find(i))
-                ans++;
+        for(int i = 0; i < g.size(); i++){
+            for(int j = 0; j < g[0].size(); j++){
+                ans += dfs(g,i,j) ? 1 : 0;
+            }
         }
         return ans;
-    }   
+    }
+    int dfs(vector<vector<int>>& g, int i, int j){
+        if(min(i,j) < 0 || max(i,j) >= g.size() || g[i][j] != 0){
+            return 0;
+        }
+        g[i][j] = 1;
+        return 1 + dfs(g,i+1,j) + dfs(g,i-1,j) + dfs(g,i,j+1) + dfs(g,i,j-1);
+    }
 };
