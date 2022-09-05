d2608c14e99914ac9e028cd31c58d47ef4a3017b
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-05 14:36:52 +0900

    Time: 171 ms (81.15%), Space: 84.9 MB (20.83%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        unordered_set<int> dp;
-        dp.insert(0);
-        int totalSum = 0;
+        int sum = 0;
         for(int i = 0; i < nums.size(); i++){
-            totalSum+=nums[i];
+            sum+=nums[i];
         }
-        if(totalSum % 2 != 0)return false;
-        int target = totalSum/2;
-        for(int i = nums.size()-1; i >= 0; i--){
-            unordered_set<int> nextdp;
-            nextdp.insert(nums[i]);
-            for(auto t: dp){
-                if(t+nums[i] == target) return true;
-                nextdp.insert(t+nums[i]);
-                nextdp.insert(t);
-            }
-            dp = nextdp;
+        if(sum%2 != 0) return false;
+        sum /=2;
+        vector<vector<int>> memo(nums.size(),vector<int>(sum+1,-1));
+        if(helper(0,sum,nums,memo)){
+            return true;
         }
         return false;
+    }
+    bool helper(int start, int sum, vector<int>& nums, vector<vector<int>>& memo){
+        if(sum == 0) return true;
+        if(sum < 0 || start == nums.size()) return false;
+        if(memo[start][sum] != -1) return memo[start][sum];
+        bool result = helper(start+1,sum-nums[start],nums,memo) || helper(start+1,sum,nums,memo);
+        memo[start][sum] = result;
+        return result;
     }
 };
