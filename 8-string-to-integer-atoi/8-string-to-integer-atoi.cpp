c10ab70d5a6dfe3dd79db29dc7f9faf42b5337bc
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-05 19:10:54 +0900

    Time: 14 ms (5.50%), Space: 7.1 MB (53.55%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int result = 0;
+        int to_compute = 1;
+        if(s == "0") return 0;
+        if(isalpha(s[0])) return 0;
+        if(s[0] == '.')return 0;
+        int error = 0;
+        int zero_count = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i]-'0' == 0){
+                zero_count++;
+            }
+            if(s[i] == ' '){
+                if(result != 0) return result;
+                if(error > 0 and result == 0) return 0;
+                if(zero_count > 0 and result == 0) return 0;
+                continue;
+            }
+            //if(s[i] == '0' and result == 0) continue; 
+            if(result != 0 and s[i] == '-'){
+                return result;
+            }
+            if(result != 0 and s[i] == '+'){
+                return result;
+            }
+            if(result != 0 and s[i] =='.'){
+                return result;
+            }
+            if(s[i] == '-'){
+                to_compute = -to_compute;
+                error++;
+                if(zero_count > 0) return 0;
+                if(error == 2) return 0;
+            }
+            if(s[i] == '+'){
+                error++;
+                if(error == 2) return 0;
+            }
+            if(result != 0 and isalpha(s[i])){
+                return result;
+            }
+            if(result == 0 and isalpha(s[i])){
+                return result;
+            }
+            if(s[i]-'0' >= 0 and s[i]-'0' <= 9){
+                if(result > INT_MAX/10 || (result == INT_MAX/10 and (s[i]-'0') > INT_MAX%10)){
+                    return INT_MAX;
+                }
+                if(result < INT_MIN/10 || (result == INT_MIN/10 and -(s[i]-'0') < INT_MIN%10)){
+                    return INT_MIN;
+                }
+                result = result * 10 + (s[i]-'0') * to_compute;
+            }
+        }
+        return result;
+    }
+};
