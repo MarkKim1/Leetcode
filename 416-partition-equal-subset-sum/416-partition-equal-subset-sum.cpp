764aa164a6476f01e1d7182a8ac79d0b60063b24
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-05 15:45:18 +0900

    Time: 1397 ms (5.01%), Space: 246.8 MB (5.00%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -6,19 +6,16 @@ public:
             sum+=nums[i];
         }
         if(sum%2 != 0) return false;
-        sum /=2;
-        vector<vector<int>> memo(nums.size(),vector<int>(sum+1,-1));
-        if(helper(0,sum,nums,memo)){
-            return true;
+        sum/=2;
+        set<int> dp;
+        dp.insert(0);
+        for(int i = nums.size()-2; i >=0; i--){
+            set<int> temp = dp;
+            for(auto a : temp){
+                if(a + nums[i] == sum) return true;
+                dp.insert(a + nums[i]);
+            }
         }
         return false;
-    }
-    bool helper(int start, int sum, vector<int>& nums, vector<vector<int>>& memo){
-        if(sum == 0) return true;
-        if(sum < 0 || start == nums.size()) return false;
-        if(memo[start][sum] != -1) return memo[start][sum];
-        bool result = helper(start+1,sum-nums[start],nums,memo) || helper(start+1,sum,nums,memo);
-        memo[start][sum] = result;
-        return result;
     }
 };
