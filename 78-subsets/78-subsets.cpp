1f87dbc0b5b55a9b2dd41b34c863142e80007115
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-06 18:30:44 +0900

    Time: 7 ms (36.05%), Space: 7.3 MB (57.19%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -2,38 +2,18 @@ class Solution {
 public:
     vector<vector<int>> result;
     vector<int> sub;
-    int other = 0;
     vector<vector<int>> subsets(vector<int>& nums) {
-        //result.push_back(sub);
-        find(nums,0);
+        result.push_back(sub);
+        helper(0,nums,sub);
         return result;
     }
     
-    void find(vector<int>&nums, int i){
-        result.push_back(sub);   
-        for(int j = i; j < nums.size(); j++){
-            sub.push_back(nums[j]);
-            find(nums,j+1);
-            sub.pop_back();
-            }
+   void helper(int i, vector<int>& nums,vector<int>& sub){
+       for(int n = i; n < nums.size(); n++){
+           sub.push_back(nums[n]);
+           result.push_back(sub);
+           helper(n+1,nums,sub);
+           sub.pop_back();
         }
+    }
 };
-
-// class Solution {
-// public:
-//     vector<vector<int>> subsets(vector<int>& nums) {
-//         vector<vector<int>> subs;
-//         vector<int> sub;
-//         subsets(nums, 0, sub, subs);
-//         return subs;
-//     }
-// private:
-//     void subsets(vector<int>& nums, int i, vector<int>& sub, vector<vector<int>>& subs) {
-//         subs.push_back(sub);
-//         for (int j = i; j < nums.size(); j++) {
-//             sub.push_back(nums[j]);
-//             subsets(nums, j + 1, sub, subs);
-//             sub.pop_back();
-//         }
-//     }
-// };
