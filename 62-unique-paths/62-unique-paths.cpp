c37c64775d9ecce1065d19fcc830616c4e3f67e2
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-06 19:28:50 +0900

    Time: 3 ms (44.69%), Space: 6.5 MB (27.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,1));
+        vector<vector<int>> result(m,vector<int>(n));
+        for(int i = 0; i < n; i++){
+            result[0][i] = 1;
+        }
+        for(int i = 0; i < m; i++){
+            result[i][0] = 1;
+        }
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                result[i][j] = result[i-1][j] + result[i][j-1];
             }
         }
-        return dp[m-1][n-1];
+        return result[m-1][n-1];
     }
 };
