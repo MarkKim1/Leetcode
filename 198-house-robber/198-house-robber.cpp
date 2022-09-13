db6c959103a374ea17f9d45d63d12ff508824596
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-13 18:54:04 +0900

    Time: 0 ms (100.00%), Space: 7.9 MB (25.89%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if(nums.size() == 1) return nums[0];
+        if(nums.size() == 1){
+            return nums[0];
+        }
         vector<int> dp(nums.size(),0);
         dp[0] = nums[0];
-        dp[1] = nums[1];
         if(nums.size() == 2){
-            return max(dp[0],dp[1]);
+            return max(nums[0],nums[1]);
         }
-        dp[2] = dp[0] + nums[2];
-        for(int i = 3; i < nums.size(); i++){
-            dp[i] = max(dp[i-2],dp[i-3]) + nums[i];
+        
+        dp[1] = max(nums[0],nums[1]);
+        
+        int result = dp[1];
+        
+        for(int i = 2; i < nums.size(); i++){
+            dp[i] = max(dp[i-2] + nums[i],dp[i-1]);
+            result = max(dp[i],result);
         }
-        return max(dp[nums.size()-1],dp[nums.size()-2]);
+        return result;
     }
 };
