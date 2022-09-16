05e4d18c0cceded78569a8e6c1d666d15298cf5b
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-16 22:04:39 +0900

    Time: 15 ms (78.87%), Space: 10.5 MB (67.09%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> dp(nums.size(),1);
-        for(int i = 1; i < nums.size(); i++){
-            for(int j = 0; j < i; j++){
-                if(nums[i] > nums[j]){
-                    dp[i] = max(dp[i],dp[j] +1);
-                }
+        vector<int> result;
+        for(int i = 0; i < nums.size(); i++){
+            if(result.empty() or result.back() < nums[i]){
+            result.push_back(nums[i]);
+            }else{
+                auto it = lower_bound(result.begin(),result.end(),nums[i]);
+                *it = nums[i];
             }
         }
-        int result = INT_MIN;
-        for(int a : dp){
-            result = max(a,result);
-        }
-        return result;
+        return result.size();
     }
 };
