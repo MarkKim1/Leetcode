cba90afe740b99778f60c4f5e67ab627fc8aafe3
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-19 00:22:32 +0900

    Time: 2409 ms (6.56%), Space: 51.2 MB (20.32%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,46 +1,24 @@
-class uf{
-    public:
-    unordered_map<int,int>parent,rank;
-    uf(vector<int>&nums){
-        for(auto & i:nums){
-            parent[i]=i;
-            rank[i]=1;
-        }
-    }
-    int find(int ele){
-        if(parent[ele]==ele)
-            return ele;
-        return parent[ele]=find(parent[ele]);
-    }
-    void un(int a,int b){
-        int p1=find(a),p2=find(b);
-        if(p1==p2)
-            return;
-        if(rank[p2]>=rank[p1])
-            swap(p2,p1);
-        parent[p2]=p1;
-        rank[p1]=rank[p1]+rank[p2];
-    }
-    int ans(){
-        int res=0;
-        for(auto & [u,v]:rank)
-          res=max(res,v);  
-        return res;
-    }
-};
-
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        uf ob(nums);
-        unordered_set<int>seen;
-        for(auto & i:nums){
-            if(seen.count(i-1))
-                ob.un(i,i-1);
-            if(seen.count(i+1))
-                ob.un(i,i+1);
-            seen.insert(i);
+        set<int> st;
+        for(int i = 0; i < nums.size(); i++){
+            st.insert(nums[i]);
         }
-        return ob.ans();
+        int result = 0;
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++){
+            int temp = nums[i];
+            if(!st.count(temp-1)){
+                result++;
+                while(st.count(temp+1)){
+                    result++;
+                    temp+=1;
+                }
+                ans = max(ans,result);
+            }
+            result = 0;
+        }
+        return ans;
     }
 };
