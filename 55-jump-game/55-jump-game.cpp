9a3733ab6b72cda1f9dda01439a1a04dda894dce
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-22 23:32:26 +0900

    Time: 186 ms (26.49%), Space: 48.5 MB (24.70%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,29 +1,14 @@
-enum Index{
-    GOOD, BAD, UNKNOWN
-};
 class Solution {
 public:
-    
     bool canJump(vector<int>& nums) {
-        vector<Index> memo(nums.size());
-        for(int i = 0; i < memo.size(); i++){
-            memo[i] = UNKNOWN;
-        }
-        memo[memo.size()-1] = GOOD;
-        return canJumpFromPosition(0,nums,memo);
-    }
-    bool canJumpFromPosition(int i, vector<int>& nums,vector<Index>& memo){
-        if(memo[i] != UNKNOWN){
-            return memo[i] == GOOD ? true : false;
-        }
-        int jump = min(i + nums[i],(int)nums.size()-1);
-        for(int j = i+1; j <= jump; j++){
-            if(canJumpFromPosition(j,nums,memo)){
-                memo[i] = GOOD;
+        int reach = 0;
+        for(int i = 0; i <= reach; i++){
+            int jump = max(i + nums[i],reach);
+            if(jump >= nums.size()-1){
                 return true;
             }
+            reach = jump;
         }
-        memo[i] = BAD;
         return false;
     }
 };
