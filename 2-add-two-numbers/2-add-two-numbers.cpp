6a1cc9cda0e7cfdea77894e0dcced101ce5b3ca7
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-23 00:38:37 +0900

    Time: 98 ms (5.12%), Space: 71.5 MB (51.84%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,32 +11,24 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *list1 = l1;
-        ListNode *list2 = l2;
-        ListNode *list3 = new ListNode();
-        ListNode *head = list3;
-        int a = 0,b = 0,c = 0;
-        int sum = 0;
-        while(list1 != NULL || list2 != NULL){
-            a = (list1 != NULL) ? list1->val : 0;
-            b = (list2 != NULL) ? list2->val : 0;
-            sum = a+b+c;//7,10,8
-            
-            if(sum > 9){
-                list3->next = new ListNode(sum%10); // 0
-                c = 1;
+        ListNode* result = new ListNode();
+        ListNode* head = result;
+        int round = 0,sum = 0;
+        while(l1 or l2){
+            int a = l1 ? l1->val : 0;
+            int b = l2 ? l2->val : 0;
+            if(a+b+sum < 10){
+                result->next = new ListNode(a+b + sum);
+            }else{
+                result->next = new ListNode(((a+b+sum)%10));
             }
-            else{
-                list3->next = new ListNode(sum);//7,8
-                c = 0;
-            }
-            
-            list3 = list3 ->next;
-            if(list1 != NULL) list1 = list1->next;
-            if(list2 != NULL) list2 = list2->next;
+            sum = (a+b+sum) / 10;
+            if(l1)l1 = l1->next;
+            if(l2)l2 = l2->next;
+            result = result->next;
         }
-        if(c == 1){
-            list3->next = new ListNode(c%10);
+        if(sum != 0){
+            result->next = new ListNode(1);
         }
         return head->next;
     }
