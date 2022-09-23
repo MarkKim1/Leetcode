9d05077a2cfb82af1b839487ca915d23e6966c92
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-23 20:53:25 +0900

    Time: 26 ms (79.33%), Space: 12.2 MB (69.66%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,26 +1,43 @@
+class DSU{
+    public:
+    void find_root(unordered_set<int>&);
+    vector<int> root;
+    vector<int> rank;
+    DSU(int x){
+        for(int i = 0; i < x; i++){
+            root.push_back(i);
+            rank.push_back(1);
+        }
+    }
+    void Union(int a, int b){
+        int rootx = find(a);
+        int rooty = find(b);
+        if(rootx != rooty){
+            root[rooty] = rootx;
+        }
+    }
+    int find(int x){
+        if(x == root[x]){
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+};
+void DSU::find_root(unordered_set<int>& set){
+    for(int i = 0; i < root.size(); i++){
+        set.insert(find(root[i]));
+    }
+}
 class Solution {
 public:
     int countComponents(int n, vector<vector<int>>& edges) {
-        vector<int> root(n,0);
-        set<int> set;
-        for(int i = 0; i < n; i++){
-            root[i] = i;
-        }
+        //sort(edges.begin(),edges.end());
+        DSU* dsu = new DSU(n);
+        unordered_set<int> set;
         for(int i = 0; i < edges.size(); i++){
-            int rootx = find(edges[i][0],root);
-            int rooty = find(edges[i][1],root);
-            if(rootx != rooty){
-                root[rooty] = rootx;
-                n-=1;
-            }
+            dsu->Union(edges[i][0],edges[i][1]);
         }
-        return n;
-        
-    }
-    int find(int vertex, vector<int>& root){
-        if(vertex == root[vertex]){
-            return vertex;
-        }
-        return root[vertex] = find(root[vertex],root);
+        dsu->find_root(set);
+        return set.size();
     }
 };
