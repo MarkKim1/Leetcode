3a45187d70143307f8a01c15997c13b641e44a2c
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-24 21:11:07 +0900

    Time: 25 ms (47.65%), Space: 17.5 MB (71.09%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -9,26 +9,10 @@ public:
             if(ans.empty() || as[i] > 0 || ans.back() < 0){
                 ans.push_back(as[i]);
             }
-            else if(as[i] < 0 && ans.back() == -as[i]){
-                ans.pop_back();   
+            else if(as[i] < 0 and ans.back() == -as[i]){
+                ans.pop_back();
             }
         }
         return ans;
     }
 };
-
-// class Solution {
-// public:
-//     vector<int> asteroidCollision(vector<int>& a) {
-//         vector<int> s; // use vector to simulate stack.
-//         for (int i : a) {
-//             while (!s.empty() && s.back() > 0 && s.back() < -i)
-//                 s.pop_back();
-//             if (s.empty() || i > 0 || s.back() < 0)
-//                 s.push_back(i);
-//             else if (i < 0 && s.back() == -i)
-//                 s.pop_back();
-//         }
-//         return s;
-//     }
-// };
