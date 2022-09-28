a25e64fdc526b365410a2a8b0ca9396cec972b43
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-09-29 04:59:57 +0900

    Time: 0 ms (100.00%), Space: 11.2 MB (83.35%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool equationsPossible(vector<string>& equations) {
+        constexpr int SIZE = 26;
+        vector<int> root(SIZE);
+        iota(root.begin(), root.end(), 0);
+
+        function<int(int)> find = [&](int x) {
+            if (root[x] != x) {
+                root[x] = find(root[x]);
+            }
+            return root[x];
+        };
+
+        auto unionSet = [&](int x, int y) {
+            int rx = find(x), ry = find(y);
+            root[rx] = ry;
+        };
+
+        for (string& eqn : equations) {
+            if (eqn[1] == '=') {
+                int x = eqn[0] - 'a';
+                int y = eqn[3] - 'a';
+                unionSet(x, y);
+            }
+        }
+
+        for (string& eqn : equations) {
+            if (eqn[1] == '!') {
+                int x = eqn[0] - 'a';
+                int y = eqn[3] - 'a';
+                if (find(x) == find(y)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
