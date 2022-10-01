71b97b3afcce0e3ee81873d2cad0538901fdd253
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-10-01 23:32:04 +0900

    Time: 7 ms (23.68%), Space: 6.3 MB (49.09%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    map<int,int> memo;
     int numDecodings(string s) {
-        return DFS(0,s);
-    }
-    int DFS(int i, string s){
-        if(memo.find(i) != memo.end()){
-            return memo[i];
-        }
-        if(i >= s.size()){
-            return 1;
-        }
-        if(s[i] == '0'){
-            return 0;
+        vector<int> dp(s.size()+1,0);
+        if (s.back() == '0'){
+            if(s.size() == 1) return 0;
+        }else{
+            if(s.size() == 1) return 1;
+            dp[s.size()-1] = 1;
         }
         
-        if(i == s.size()-1){
-            return 1;
+        for(int i = s.size()-2; i >= 0; i--){
+            if(s[i] == '0') continue;
+            if(stoi(s.substr(i,2)) <= 26){
+                if(i == s.size()-2){
+                    dp[i] = dp[i+1] + 1;
+                }else{
+                    dp[i] = dp[i+1] + dp[i+2];
+                }
+            }else{
+                dp[i] = dp[i+1];
+            }
         }
-        int ans = DFS(i+1,s);
-        if(stoi(s.substr(i,2)) <= 26){
-            ans += DFS(i+2,s);
-        }
-        memo[i] = ans;
-        return ans;
+        return dp[0];
     }
 };
