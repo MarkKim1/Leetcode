743099075cd9dca8b9a83524f46529d6a17c8eaf
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-10-01 22:57:50 +0900

    Time: 6 ms (29.19%), Space: 7.9 MB (11.16%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    unordered_map<int,int> map;
+    map<int,int> memo;
     int numDecodings(string s) {
-        return recursiveWithMemo(0,s);
+        return DFS(0,s);
     }
-    int recursiveWithMemo(int i, string s){
-        if(i == s.size()) return 1;
-        
-        if(map.find(i) != map.end()){
-            return map[i];
+    int DFS(int i, string s){
+        if(memo.find(i) != memo.end()){
+            return memo[i];
         }
         if(s[i] == '0'){
             return 0;
         }
+        if(i >= s.size()){
+            return 1;
+        }
         if(i == s.size()-1){
             return 1;
         }
-        int ans = recursiveWithMemo(i+1,s);
+        int ans = DFS(i+1,s);
         if(stoi(s.substr(i,2)) <= 26){
-            ans+=recursiveWithMemo(i+2,s);
+            ans += DFS(i+2,s);
         }
-        map[i] = ans;
+        memo[i] = ans;
         return ans;
     }
 };
