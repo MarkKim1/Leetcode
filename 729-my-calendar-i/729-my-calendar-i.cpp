e1afa52153133a1aab061b839ac8893ccd47ceba
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-10-07 19:58:08 +0900

    Time: 204 ms (41.61%), Space: 38.7 MB (66.91%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -3,8 +3,16 @@ class MyCalendar {
 public:
     bool book(int s, int e) {
         auto next = books.lower_bound({s, e}); // first element with key not go before k (i.e., either it is equivalent or goes after).
-        if (next != books.end() && next->first < e) return false; // a existing book started within the new book (next)
-        if (next != books.begin() && s < (--next)->second) return false; // new book started within a existing book (prev)
+        if (next != books.end()){
+            if(next->first < e){
+                return false;
+            }
+        }
+        if (next != books.begin()){
+            if(s < (--next)->second){
+                return false;
+            }
+        }
         books.insert({ s, e });
         return true;
     }
