0859b6c935332067ca923d6ee956c006e8948d86
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-10-07 19:55:32 +0900

    Time: 137 ms (75.60%), Space: 38.9 MB (45.58%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,18 +1,11 @@
 class MyCalendar {
+    set<pair<int, int>> books;
 public:
-    map<int,int>events;
-    MyCalendar() {   
-    }
-    bool book(int start, int end) {
-        auto next = events.upper_bound(start);
-        if(next != events.end() &&   (*next).second < end)return false;
-        events.insert({end,start});
+    bool book(int s, int e) {
+        auto next = books.lower_bound({s, e}); // first element with key not go before k (i.e., either it is equivalent or goes after).
+        if (next != books.end() && next->first < e) return false; // a existing book started within the new book (next)
+        if (next != books.begin() && s < (--next)->second) return false; // new book started within a existing book (prev)
+        books.insert({ s, e });
         return true;
     }
 };
-
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar* obj = new MyCalendar();
- * bool param_1 = obj->book(start,end);
- */
