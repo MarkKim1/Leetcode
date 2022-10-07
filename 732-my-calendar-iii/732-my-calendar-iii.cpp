e406ed0ec97c347db206252055021a014e2d740b
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-10-07 15:11:58 +0900

    Time: 129 ms (84.17%), Space: 26.3 MB (94.41%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.cpp
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -1,17 +1,18 @@
 class MyCalendarThree {
 private:
-    map<int, int> diff;
+    map<int, int> starts;
+    int res;
 
 public:
-    MyCalendarThree() {}
-
+    MyCalendarThree() {
+        starts[0] = 0, starts[1e9 + 1] = 0;
+        res = 0;
+    }
+    void split(int x) { starts[x] = (--starts.upper_bound(x))->second; }
     int book(int start, int end) {
-        int cur = 0, res = 0;
-        diff[start]++;
-        diff[end]--;
-        for (auto& delta : diff) {
-            cur += delta.second;
-            res = max(res, cur);
+        split(start), split(end);
+        for (auto it = starts.find(start); it->first < end; it++) {
+            res = max(res, ++(it->second));
         }
         return res;
     }
