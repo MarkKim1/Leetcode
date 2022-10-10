4850771abd17a8b50505c9b6a0117ca1702e6f1b
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-10-10 21:43:54 +0900

    Time: 0 ms (100.00%), Space: 6.2 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -1,24 +1,14 @@
 class Solution {
 public:
     string breakPalindrome(string p) {
-        int left = 0;
-        int right = p.size()-1;
         if(p.size() == 1) return "";
-        vector<string> result;
-        while(left < right){
-            string source = p;
-            if(source[left] == 'a' and source[right] == 'a'){
-                source[right] = 'b';
-            }else if(source[left] == 'z' and source[right] == 'z'){
-                source[left] = 'a';
-            }else{
-                source[left] = 'a';
+        for(int i = 0; i < p.size()/2; i++){
+            if(p[i] != 'a'){
+                p[i] = 'a';
+                return p;
             }
-            result.push_back(source);
-            left++;
-            right--;
         }
-        sort(result.begin(),result.end());
-        return result[0];
+        p[p.size()-1] = 'b';
+        return p;
     }
 };
