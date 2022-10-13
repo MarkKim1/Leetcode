9a72d09f52a4c62a8fafc9bc5eb7e6da1b7c7345
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-10-13 22:32:28 +0900

    Time: 226 ms (46.19%), Space: 16.5 MB (40.76%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.swift
+++ 0049-group-anagrams/0049-group-anagrams.swift
@@ -3,22 +3,8 @@ class Solution {
         var map = [String: [String]]()
         for str in strs {
             var sorted = String(str.sorted())
-            map[sorted,default: []].append(str)
+            map[sorted,default:[]].append(str)
         }
         return Array(map.values)
     }
 }
-
-// class Solution {
-//     func groupAnagrams(_ strs: [String]) -> [[String]] {
-//         if strs.isEmpty { return [] }
-        
-//         var map = [String: [String]]()
-//         for str in strs {
-//             let sortedStr = String(str.sorted())
-//             map[sortedStr, default: []] += [str]
-//         }
-                
-//         return Array(map.values)
-//     }
-// }
