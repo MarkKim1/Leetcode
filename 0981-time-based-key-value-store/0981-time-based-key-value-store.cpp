72eb87aa8545e77115ca1a1b9e3e67c954454766
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-10-23 16:46:13 +0900

    Time: 877 ms (15.82%), Space: 131.2 MB (17.98%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -0,0 +1,16 @@
+class TimeMap {
+public:
+    unordered_map<string,map<int,string>> map;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        map[key].insert(make_pair(timestamp,value));
+    }
+    
+    string get(string key, int timestamp) {
+        auto it = map[key].upper_bound(timestamp);
+        return it == map[key].begin() ? "" : prev(it)->second;
+    }
+};
