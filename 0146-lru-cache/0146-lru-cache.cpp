8323d281787c93db11f4caba9fbf6d1e4ffd0600
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-10-24 17:02:25 +0900

    Time: 660 ms (35.44%), Space: 178.7 MB (16.21%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,92 +1,68 @@
 class LRUCache {
 public:
-    
-    class node{
+    class node {
         public:
         int key;
         int val;
         node* next;
         node* prev;
         
-        node( int key , int val)
-        {
+        node(int key, int val){
             this->key = key;
-            this->val=val;
-            this->next=NULL;
-            this->prev=NULL;
-            
+            this->val = val;
+            this->next = NULL;
+            this->prev = NULL;
         }
     };
-
-    unordered_map<int,node*>mp;
+    unordered_map<int,node*>map;
     int capacity;
-   
-//two dummy nodes to keep track starting and ending point of list  
-    node * head = new node(0,0);
-    node * tail = new node(0,0);
+    node* head = new node(0,0);
+    node* tail = new node(0,0);
     
-
-    
-    LRUCache(int capacity)
-    {
+    LRUCache(int capacity) {
         this->capacity = capacity;
-        head->next=tail;
-        tail->prev= head;
+        head->next = tail;
+        tail->prev = head;
         
     }
-    
-// function to help a node in the next to head [can say front of our list]
-    void insert ( node* currnode)
-    {
-        currnode->next = head->next;                      // adding node in next of head (front of our cache)
+    void insert(node* currnode){
+        currnode->next = head->next;
         head->next->prev = currnode;
-        head->next= currnode;
-        currnode->prev= head;
-        mp[currnode->key]=currnode;                       //update map accordingly
+        head->next = currnode;
+        currnode->prev = head;
+        map[currnode->key] = currnode;
     }
     
-   
-// function to remove a node 
-    void remove(node* currnode)
-    {
-        mp.erase(currnode->key);
-        currnode->next->prev =currnode->prev;
-        currnode->prev->next=currnode->next;
-       
+    void remove(node* currnode){
+        map.erase(currnode->key);
+        currnode->next->prev = currnode->prev;
+        currnode->prev->next = currnode->next;
     }
-  
-  
-    int get(int key) 
-    {
-       // if key is not present than return -1;
-        if(mp.find(key)==mp.end())
-        {
+    
+    int get(int key) {
+        if(map.find(key) == map.end()){
             return -1;
         }
-        //if key is present , than update the node and return the value associated with the key
-        // for update first we remove the node and then add at start of list with the help of insert function
-        node *currnode = mp[key];
+        node* currnode = map[key];
         remove(currnode);
         insert(currnode);
         return currnode->val;
-        
     }
     
-    void put(int key, int value)
-    {
-       //if key is already present than we have to update it 
-       // similiary we first remove and then add node in front with the help of insert helper 
-        if(mp.find(key)!=mp.end())
-        {
-            remove(mp[key]);
+    void put(int key, int value) {
+        if(map.find(key) != map.end()){
+            remove(map[key]);
         }
-        
-        // if size is full then we have to remove the least recently used node
-        if(mp.size()==capacity)
-        {
+        if(map.size() == capacity){
             remove(tail->prev);
         }
         insert(new node(key,value));
-        
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
