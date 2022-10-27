52ce4b7f3faef3c8c0430d472512a96d76eebafc
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-10-28 02:17:24 +0900

    Time: 25 ms (45.24%), Space: 12.3 MB (11.98%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
+        unordered_map<string,function<long long (long long,long long) > >map = {
+            {"+",[](long long a, long long b) {return a+b;}},
+            {"*",[](long long a, long long b) {return a*b;}},
+            {"/",[](long long a, long long b) {return a/b;}},
+            {"-",[](long long a, long long b) {return a-b;}}
+        };
         stack<long long> stk;
-        int curr = 0;
-        bool neg = false;
-        for(int i = 0; i < tokens.size(); i++){
-            if(tokens[i] != "+" and tokens[i] != "-" and tokens[i] != "/" and tokens[i] != "*"){    
-                for(int j = 0; j < tokens[i].size(); j++){
-                    if(tokens[i][j] == '-'){
-                        neg = true;
-                    }else{
-                        curr = curr * 10 + (tokens[i][j]-'0');
-                    }
-                }
-                if(neg) curr = -curr;
-                stk.push(curr);
-                curr = 0;
-                neg = false;
+        
+        for(auto s : tokens){
+            if(map.find(s) == map.end()){
+                stk.push(stoi(s));
             }else{
                 long long second = stk.top();
                 stk.pop();
                 long long first = stk.top();
                 stk.pop();
-                if(tokens[i] == "+"){
-                    stk.push(first + second);
-                }
-                if(tokens[i] == "-"){
-                    stk.push(first-second);
-                }
-                if(tokens[i] == "/"){
-                    stk.push(first/second);
-                }
-                if(tokens[i] == "*"){
-                    stk.push(first*second);
-                }
+                stk.push(map[s](first,second));
             }
-            
         }
         return stk.top();
     }
