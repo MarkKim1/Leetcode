83ba9868fcec13e7a176d104b691c8fc823ddc34
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-09 05:53:31 +0900

    Time: 12 ms (87.24%), Space: 10.1 MB (71.27%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -2,17 +2,29 @@ class Solution {
 public:
     
     vector<int> sortByBits(vector<int>& arr) {
-        auto comparator = [](int a, int b){
-            bitset<32> aBit(a);
-            bitset<32> bBit(b);
-            int acount = aBit.count();
-            int bcount = bBit.count();
-            if(acount == bcount){
-                return a < b;
-            }else{
-                return acount < bcount;
+           auto comparator = [](int &x, int &y) {
+            int x_1 = 0;
+            int y_1 = 0;
+            
+            int temp_x = x;
+            int temp_y = y;
+            
+            while(temp_x) {
+                temp_x = temp_x & (temp_x-1);
+                x_1++;
             }
+            
+            while(temp_y) {
+                temp_y = temp_y & (temp_y-1);
+                y_1++;
+            }
+            
+            if(x_1 == y_1)
+                return x < y;
+            
+            return x_1<y_1;
         };
+        
         sort(arr.begin(),arr.end(),comparator);
         return arr;
     }
