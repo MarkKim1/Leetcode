7b68419fbb930ff6eb21fe7efe1bc46e53810990
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-09 06:58:12 +0900

    Time: 73 ms (65.38%), Space: 31 MB (21.93%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -5,10 +5,7 @@ public:
         for(int i = 0; i < nums.size(); i++){
             map[nums[i]]++;
         }
-        int count = 1;
-        vector<int> ans;
-        int dup = -1;
-        int loss = -1;
+        int count = 1,dup = -1,loss = -1;
         for(int i = 0; i < nums.size(); i++){
             if(map.find(count) != map.end() and map[count] > 1){
                 dup = count;
