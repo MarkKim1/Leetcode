45f683d5f05f76a53866a120f848e475cd033c84
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-09 16:36:44 +0900

    Time: 370 ms (22.35%), Space: 57.6 MB (59.51%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
-    vector<string> curr;
     vector<vector<string>> partition(string s) {
-        DFS(s,0);
+        vector<vector<string>> ans;
+        vector<string> curr;
+        DFS(ans,curr,s,0);
         return ans;
     }
-    void DFS(string s, int start){
-        if(start >= s.length()){
+    void DFS(vector<vector<string>>& ans, vector<string>& curr, string s, int start){
+        if(start >= s.size()){
             ans.push_back(curr);
         }
-        for(int end = start; end < s.length(); end++){
-            if(ispalindrome(s,start,end)){
+        for(int end = start; end < s.size(); end++){
+            if(isPalindrome(s,start,end)){
                 curr.push_back(s.substr(start,end-start+1));
-                DFS(s,end+1);
+                DFS(ans,curr,s,end+1);
                 curr.pop_back();
             }
         }
     }
-    bool ispalindrome(string s, int left, int right){
+    bool isPalindrome(string s, int left, int right){
         while(left < right){
             if(s[left++] != s[right--]){
                 return false;
