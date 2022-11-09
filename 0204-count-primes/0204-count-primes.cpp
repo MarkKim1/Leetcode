bcbe383575bdf6226e556798c54e35da21122daa
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-09 21:21:06 +0900

    Time: 185 ms (95.36%), Space: 10.1 MB (92.36%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,46 +1,21 @@
-// class Solution {
-// public:
-//     int countPrimes(int n) {
-//         if(n == 1 || n == 0) return 0;
-//         vector<bool> arr(n,false);
-//         int ans = 0;
-//         for(int i = 2; i*i < arr.size(); i++){
-//             if(arr[i] == false){
-//                 for(int j = i*i; j < arr.size(); j+=i){
-//                     arr[j] = true;
-//                 }
-//             }
-//         }
-//         for(int i = 2; i < arr.size(); i++){
-//             if(arr[i] == false){
-//                 ans++;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
 class Solution {
 public:
-	int countPrimes(int n) {
-		//prime no. generation using sieve of eratothons
-		vector<bool> prime(n + 1, true);
-		prime[0] = false;
-		prime[1] = false;
-		for (int i = 2; i * i <= n; i++) {
-			if (prime[i]) {
-				for (int j = i * i; j <= n; j += i) {
-					prime[j] = false;
-				}
-			}
-		}
-		//counting prime numbers
-		int primeCount = 0;
-		for (int i = 2; i < n; i++) {
-			if (prime[i]) {
-				primeCount++;
-			}
-		}
-		return primeCount;
-	}
+    int countPrimes(int n) {
+        if(n == 1 || n == 0) return 0;
+        vector<bool> arr(n,false);
+        for(int i = 2; i*i < n; i++){
+            if(!arr[i]){
+                for(int j = i*i; j < n; j+=i){
+                    arr[j] = true;
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 2; i < n; i++){
+            if(arr[i] == false){
+                ans++;
+            }
+        }
+        return ans;
+    }
 };
