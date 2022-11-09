d0af4e3dae7eb14c64dcc8f4df094dfa1c8691c9
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-09 21:59:16 +0900

    Time: 7 ms (84.60%), Space: 6.6 MB (56.42%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        string curr = "1";
+        string count_to_say = "1";
         while(--n){
             string temp = "";
-            for(int i = 0; i < curr.size(); i++){
+            for(int i = 0; i < count_to_say.size(); i++){
                 int count = 1;
-                while(i+1 < curr.size() and curr[i] == curr[i+1]){
+                while(i + 1 < count_to_say.size() and count_to_say[i] == count_to_say[i+1]){
                     count++;
                     i++;
                 }
-                temp += to_string(count) + curr[i];
+                temp += to_string(count) + count_to_say[i];
             }
-            curr = temp;
+            count_to_say = temp;
         }
-        return curr;
+        return count_to_say;
     }
 };
