9666c6099748f7125b05b5063995c2aec1c71512
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-11 07:39:10 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (54.04%) - LeetHub

---
+++ 2457-minimum-addition-to-make-integer-beautiful/2457-minimum-addition-to-make-integer-beautiful.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+        int sum(long long n) {
+        int res = 0;
+        while (n) {
+            res += n % 10;
+            n /= 10;
+        }
+        return res;
+    }
+
+    long long makeIntegerBeautiful(long long n, int target) {
+        long long n0 = n, base = 1;
+        while (sum(n) > target) {
+            n = n / 10 + 1;
+            base *= 10;
+        }
+        return n * base - n0;
+    }
+};
