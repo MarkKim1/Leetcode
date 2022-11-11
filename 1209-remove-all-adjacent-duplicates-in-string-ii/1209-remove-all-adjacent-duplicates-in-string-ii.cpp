93ed6b728a3b54d83b4f154d7aea9d12ac9028fe
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-11 15:35:24 +0900

    Time: 51 ms (60.11%), Space: 11 MB (30.09%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -3,21 +3,15 @@ public:
     string removeDuplicates(string s, int k) {
         vector<pair<char,int>> stk;
         for(int i = 0; i < s.size(); i++){
-            if(!stk.empty() and stk.back().first == s[i]){
-                if(stk.back().second == k-1){
-                    for(int j = 0; j < k-1; j++){
-                        stk.pop_back();
-                    }
-                }else{
-                    stk.push_back(make_pair(s[i],stk.back().second + 1));
-                }
-            }else{
-                stk.push_back(make_pair(s[i],1));
+            if(stk.empty() || stk.back().first != s[i]){
+                stk.push_back({s[i],1});
+            }else if(++stk.back().second == k){
+                stk.pop_back();
             }
         }
         string ans = "";
         for(int i = 0; i < stk.size(); i++){
-            ans.push_back(stk[i].first);
+            ans += string(stk[i].second,stk[i].first);
         }
         return ans;
     }
