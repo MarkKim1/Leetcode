14c9578c0b38461709b8578787ee0b5c0b0d082e
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-11 13:16:44 +0900

    Time: 614 ms (5.70%), Space: 19.1 MB (6.21%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        map<int,int> map;
-        int count = 0;
+        unordered_map<int,int> hash;
         for(int i = 0; i < nums.size(); i++){
-            if(map[nums[i]] > 0){
-                continue;
-            }else{
-                map[nums[i]]++;
-                count++;
+            hash[nums[i]]++;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(hash[nums[i]] != 1){
+                nums.erase(nums.begin() +i);
+                hash[nums[i]]--;
+                i--;
             }
         }
-        int size = map.size();
-        int i = 0;
-        for(auto each : map){
-            nums[i] = each.first;
-            i++;
-        }
-        return count;
+        return nums.size();
     }
 };
