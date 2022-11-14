03deb5a61141cd81a975eadc314f78c08e1d8285
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-14 22:29:47 +0900

    Time: 1147 ms (5.33%), Space: 216.2 MB (5.14%) - LeetHub

---
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool containsCycle(vector<vector<char>>& grid) {
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
+                if(isalpha(grid[i][j])){
+                    if(bfs(grid,i,j,0)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    bool bfs(vector<vector<char>>& grid, int i, int j, int count){
+        vector<int> x = {0,1,0,-1};
+        vector<int> y = {1,0,-1,0};
+        queue<pair<int,int>> q;
+        q.push(make_pair(i,j));
+        char source = grid[i][j];
+        while(!q.empty()){
+            int size = q.size();
+            count++;
+            for(int i = 0; i < size; i++){
+                auto curr = q.front();
+                int row = curr.first;
+                int col = curr.second;
+                q.pop();
+                if(!q.empty() and count >= 3 and row == q.front().first and col == q.front().second){
+                    return true;
+                }
+                grid[row][col] = '#';
+                for(int i = 0; i < 4; i++){
+                    int newrow = row + x[i];
+                    int newcol = col + y[i];
+                    if(min(newrow,newcol) >= 0 and newrow < grid.size() and newcol < grid[0].size() and grid[newrow][newcol] == source){
+                        q.push(make_pair(newrow,newcol));
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
