203abbe92661c077295ace67357be6d2ae040206
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-16 22:49:41 +0900

    Time: 65 ms (80.70%), Space: 5.9 MB (74.31%) - LeetHub

--- 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool sumOfNumberAndReverse(int num) {
-    auto rev = [](int n) {
-        int rn = 0;
-        for (; n; n /= 10)
-            rn = rn * 10 + n % 10;
-        return rn;
-    };  
-    for (int n1 = num / 2; n1 <= num; ++n1)
-        if (n1 + rev(n1) == num)
-            return true;
-    return false;
-}
+        auto rev = [](int i){
+            int result = 0;
+            while(i != 0){
+                result = result * 10 + i % 10;
+                i /= 10;
+            }
+            return result;
+        };
+        for(int i = num/2; i <= num; i++){
+            if(i + rev(i) == num) return true;
+        }
+        return false;
+    }
 };
