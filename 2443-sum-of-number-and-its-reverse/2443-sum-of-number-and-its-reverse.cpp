a5d69b3fa34a6aedfb946ec51efdb297869be658
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-16 22:46:49 +0900

    Time: 1968 ms (14.03%), Space: 6 MB (5.75%) - LeetHub

--- 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool sumOfNumberAndReverse(int num) {
-        for(int i = 0; i <= num; i++){
+        for(int i = num/2; i <= num; i++){
             string a = to_string(i);
             reverse(a.begin(),a.end());
             int b = stoi(a);
