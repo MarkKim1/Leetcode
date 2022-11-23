230f522c717f62117e814c4465d58d5b4ceea17e
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-23 23:51:07 +0900

    Time: 140 ms (86.31%), Space: 30.5 MB (54.90%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        vector<int> row = {1,0,-1,0};
-        vector<int> col = {0,1,0,-1};
-        queue<pair<int,int>> q;
-        q.push({entrance[0],entrance[1]});
-        int count = 0;
+        vector<int> x = {1,0,-1,0};
+        vector<int> y = {0,1,0,-1};
+        queue<pair<pair<int,int>,int>> q;
+        q.push({{entrance[0],entrance[1]},0});
         while(!q.empty()){
             int size = q.size();
             for(int i = 0; i < size; i++){
                 auto curr = q.front();
                 q.pop();
-                int x = curr.first;
-                int y = curr.second;
-                maze[x][y] = '+';
-                if(count != 0 and (x == 0 || y == 0 || x == maze.size()-1
-                                  || y == maze[0].size()-1)){
-                    return count;
+                int row = curr.first.first;
+                int col = curr.first.second;
+                int distance = curr.second;
+                maze[row][col] = '+';
+                if((row != entrance[0] || col != entrance[1]) && (row == 0 || row == maze.size()-1 || col == 0 || col == maze[0].size()-1)){
+                    return distance;
                 }
                 for(int i = 0; i < 4; i++){
-                    int next_x = x + row[i];
-                    int next_y = y + col[i];
-                    if(min(next_x,next_y) >= 0 and next_x < maze.size() and
-                      next_y < maze[0].size() and maze[next_x][next_y] != '+'){
-                        maze[next_x][next_y] = '+';
-                        q.push({next_x,next_y});
+                    int nextrow = row + x[i];
+                    int nextcol = col + y[i];
+                    if(min(nextrow,nextcol) >= 0 && nextrow < maze.size() && nextcol < maze[0].size() && maze[nextrow][nextcol] != '+'){
+                        maze[nextrow][nextcol] = '+';
+                        q.push({{nextrow,nextcol},distance + 1});
                     }
                 }
             }
-            count++;
         }
         return -1;
     }
 };
- // ["+","0","+","+","+","+","+"],
- // ["+","0","+","0","0","0","+"],
- // ["+","0","+","0","+","0","+"],
- // ["+","0","0","0","+","0","+"],
- // ["+","+","+","+","+","0","+"]
