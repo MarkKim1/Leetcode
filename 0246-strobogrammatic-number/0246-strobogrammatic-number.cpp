debc38ab2072e2347b6fb2608a59465c464961f4
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-25 01:41:28 +0900

    Time: 0 ms (100.00%), Space: 6.1 MB (61.68%) - LeetHub

--- 0246-strobogrammatic-number/0246-strobogrammatic-number.cpp
+++ 0246-strobogrammatic-number/0246-strobogrammatic-number.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool isStrobogrammatic(string num) {
-        string strobo = "";
         for(int i = num.size()-1; i >= 0; i--){
             if(num[i] != '6' && num[i] != '9' && num[i] != '8' && num[i] != '0' && num[i] != '1'){
                 return false;
