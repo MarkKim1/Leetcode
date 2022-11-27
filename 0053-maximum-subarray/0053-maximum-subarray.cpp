a03e924b6bae5e94b2d2f77977d2a91aa6eac944
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-28 01:56:54 +0900

    Time: 310 ms (35.15%), Space: 70.6 MB (5.53%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        if(nums.size() == 1) return nums[0];
+        //if(nums.size() == 1) return nums[0];
         int n = nums.size();
         vector<int> dp(n,0);
         dp[0] = nums[0];
