da13947f27aab90b68ecc1325d5df64851d5935d
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-27 19:33:37 +0900

    Time: 0 ms (100.00%), Space: 6.8 MB (61.96%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,22 +1,33 @@
 class Solution {
-    public:
+public:
     int longestSubstring(string s, int k) {
         int n = s.size();
-        return longestSubstringUtil(s, 0, n, k);
+        return longestSubstringUtil(s,0,n,k);
     }
-    int longestSubstringUtil(string &s, int start, int end, int k) {
-        if (end < k) return 0;
-        int countMap[26] = {0};
-        // update the countMap with the count of each character
-        for (int i = start; i < end; i++)
-            countMap[s[i] - 'a']++;
-        for (int mid = start; mid < end; mid++) {
-            if (countMap[s[mid] - 'a'] >= k) continue;
-            int midNext = mid + 1;
-            while (midNext < end && countMap[s[midNext] - 'a'] < k) midNext++;
-            return max(longestSubstringUtil(s, start, mid, k),
-                    longestSubstringUtil(s, midNext, end, k));
+    int longestSubstringUtil(string s, int start, int end, int k){
+        if(start >= end){
+            return 0;
         }
-        return (end - start);
+        int countMap[26] = {0};
+        for(int i = start; i < end; i++){
+            countMap[s[i]-'a']++;
+        }
+        for(int mid = start; mid < end; mid++){
+            if(countMap[s[mid]-'a'] >= k) continue;
+            int midnext = mid+1;
+            while(midnext < end and countMap[s[midnext]-'a'] < k){
+                midnext++;
+            }
+            return max(longestSubstringUtil(s,start,mid,k),
+                       longestSubstringUtil(s,midnext,end,k));
+        }
+        return end-start;
     }
 };
+//aabbbccddd k = 3
+//a bbbccddd
+//. bbbc ddd
+//  
+//0123456789
+//[a = 2, b = 3, c = 2, d = 3]
+//
