06617ff20fe033191bb3c9bc8c6d08726d48e204
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-29 07:10:59 +0900

    Time: 1250 ms (59.69%), Space: 154.8 MB (34.21%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         int n = nums.size();
-        vector<map<long,int>> dp(n);
+        vector<unordered_map<long,int>> dp(n);
         int ans = 0;
         for(int i = 1; i < n; i++){
             for(int j = 0; j < i; j++){
