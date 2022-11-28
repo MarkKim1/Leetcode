c1fbf096cd5eab6360fecd541a4a399b50aec92c
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-11-29 07:55:13 +0900

    Time: 140 ms (43.83%), Space: 9.4 MB (46.97%) - LeetHub

--- 0823-binary-trees-with-factors/0823-binary-trees-with-factors.cpp
+++ 0823-binary-trees-with-factors/0823-binary-trees-with-factors.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int numFactoredBinaryTrees(vector<int>& arr) {
-        unordered_map<long,long> map;
-        vector<long> dp(arr.size(),1);
         sort(arr.begin(),arr.end());
-        map[arr[0]]++;
-        long ans = 1;
+        vector<long> dp(arr.size(),1);
+        unordered_map<long,long> map;
+        long ans = 0;
         int MOD = pow(10,9) + 7;
-        for(int i = 1; i < arr.size(); i++){
-            for(auto each : map){
-                if(arr[i] % each.first == 0 and map.find(arr[i] / each.first) != map.end()){
+        for(int i = 0; i < arr.size(); i++){
+            for(auto each: map){
+                if(arr[i] % each.first == 0 and map.find(arr[i]/each.first) != map.end()){
                     dp[i] += (each.second * map[arr[i]/each.first]) % MOD;
-                }
+                }   
             }
-            ans = (ans +dp[i]) % MOD;
             map[arr[i]] = dp[i];
+            ans = (ans + dp[i]) % MOD;
         }
-        return (int) ans;
+        return int(ans);
     }
 };
