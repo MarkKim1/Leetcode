37181c6593fe69dc2999bb58e283292ca198b68c
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-12-01 00:35:38 +0900

    Time: 3 ms (86.20%), Space: 8.2 MB (20.75%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -3,12 +3,15 @@ public:
     bool uniqueOccurrences(vector<int>& arr) {
         unordered_map<int,int> map;
         unordered_set<int> set;
-        for(int i = 0; i < arr.size(); i++){
+        for(int i = 0 ; i < arr.size(); i++){
             map[arr[i]]++;
         }
-        for(auto a:map){
-            set.insert(a.second);
+        for(auto each : map){
+            if(set.count(each.second)){
+                return false;
+            }
+            set.insert(each.second);
         }
-        return map.size() == set.size();
+        return true;
     }
 };
