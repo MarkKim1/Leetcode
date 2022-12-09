c53a75da2ff96af2e5251c7391ae40dac4e251cb
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-12-10 03:24:51 +0900

    Time: 0 ms (100.00%), Space: 11.1 MB (14.90%) - LeetHub

---
+++ 0364-nested-list-weight-sum-ii/0364-nested-list-weight-sum-ii.cpp
@@ -0,0 +1,31 @@
+
+class Solution {
+public:
+    int depthSumInverse(vector<NestedInteger>& nestedList) {
+        
+        int maxdepth = finddepth(nestedList);
+        cout << "maxdepth : " << maxdepth << endl;
+        return findweight(nestedList,maxdepth,1);
+    }
+    int finddepth(vector<NestedInteger>& list){
+        if(list.size() == 0 || list.empty())return 0;
+        int maxdepth = 1;
+        for(NestedInteger child : list){
+            if(!child.isInteger() and list.size() != 0){
+                maxdepth = max(maxdepth, 1 + finddepth(child.getList()));
+            }
+        }
+        return maxdepth;
+    }
+    int findweight(vector<NestedInteger>& list, int maxdepth,int depth){
+        int total = 0;
+        for(auto child : list){
+            if(child.isInteger()){
+                total += child.getInteger() * (maxdepth - depth + 1);
+            }else{
+                total += findweight(child.getList(), maxdepth, depth + 1);
+            }
+        }
+        return total;
+    }
+};
