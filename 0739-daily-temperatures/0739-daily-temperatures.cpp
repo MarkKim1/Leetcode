61a255508f7c4677173b599670d2ed8d934c16fe
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-12-18 10:23:16 +0900

    Time: 268 ms (72.53%), Space: 103.1 MB (40.54%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temp) {
-        stack<pair<int,int>> stk;
-        vector<int> ans(temp.size());
+        stack<int> stk;
+        vector<int> result(temp.size(),0);
         for(int i = 0; i < temp.size(); i++){
             if(stk.empty()){
-                stk.push({i,temp[i]});
-                continue;
+                stk.push(i);
+            }else{
+                while(!stk.empty() and temp[stk.top()] < temp[i]){
+                    result[stk.top()] = i - stk.top();
+                    stk.pop();
+                }
+                stk.push(i);
             }
-            while(!stk.empty() and temp[i] > stk.top().second){
-                ans[stk.top().first] = i-stk.top().first;
-                stk.pop();
-            }
-            stk.push({i,temp[i]});
         }
-        return ans;
+        return result;
     }
 };
