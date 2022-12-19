343972e4678df92afa200caab3ca32450ccd919f
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-12-19 11:21:05 +0900

    Time: 17 ms (9.05%), Space: 10 MB (16.89%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.cpp
+++ 0269-alien-dictionary/0269-alien-dictionary.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     string alienOrder(vector<string>& words) {
-        unordered_map<char,int> indegree;
-        unordered_map<char,unordered_set<char>> graph;
-        //initialize the indegree
-        for(int i = 0; i < words.size(); i++){
-            for(int j = 0; j < words[i].size(); j++){
-                char c = words[i][j];
-                indegree[c] = 0;
-            }
+      unordered_map<char,unordered_set<char>> graph;
+      unordered_map<char,int> indegree;
+      
+      for(int i = 0; i < words.size(); i++){
+        for(int j = 0; j < words[i].size(); j++){
+          char c = words[i][j];
+          indegree[c] = 0;
         }
-        //make a graph and modify the indegree
-        for(int i = 0; i < words.size()-1; i++){
-            string curr = words[i];
-            string next = words[i+1];
-            int len = min(curr.size(),next.size());
-            for(int j = 0; j < len; j++){
-                if(curr[j] != next[j]){
-                    unordered_set<char> set = graph[curr[j]];
-                    if(set.find(next[j]) == set.end()){
-                        indegree[next[j]]++;
-                        graph[curr[j]].insert(next[j]);
-                    }
-                    break;
-                }
-                if(j == len - 1 and curr.size() > next.size()) return "";
+      }
+      
+      for(int i = 0; i < words.size() - 1; i++){
+        string curr = words[i];
+        string next = words[i+1];
+        int len = min(curr.size(),next.size());
+        for(int j = 0; j < len; j++){
+          unordered_set<char> set = graph[curr[j]];
+          if(curr[j] != next[j]){
+            if(set.find(next[j]) == set.end()){
+              graph[curr[j]].insert(next[j]);
+              indegree[next[j]]++;
             }
-            
+            break;
+          }
+          if(j == len-1 and curr.size() > next.size()) return "";
         }
-        queue<char> q;
+      }
+       queue<char> q;
         string result = "";
         for(auto& each : indegree){
             if(each.second == 0){
