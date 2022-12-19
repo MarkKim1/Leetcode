d46959e58ff21253e7e71041bcab3ad16b6aa8dd
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2022-12-19 11:24:09 +0900

    Time: 82 ms (76.04%), Space: 57 MB (33.65%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.cpp
+++ 0384-shuffle-an-array/0384-shuffle-an-array.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    vector<int> origin;
+    vector<int> original;
     vector<int> shuffled;
     Solution(vector<int>& nums) {
-        this->origin = nums;
+        this->original = nums;
         this->shuffled = nums;
     }
     
     vector<int> reset() {
-        return origin;
+        return original;
     }
     
     vector<int> shuffle() {
         for(int i = 0; i < shuffled.size(); i++){
-            int pos = rand() % (shuffled.size()-1);
-            swap(shuffled[pos],shuffled[i]);
+            int random_index = rand() % (shuffled.size()-1);
+            swap(shuffled[i],shuffled[random_index]);
         }
         return shuffled;
     }
