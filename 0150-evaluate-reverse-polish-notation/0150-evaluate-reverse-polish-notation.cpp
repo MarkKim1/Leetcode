751d70d61868dab6c122630d6ef5cc43b4dbb40d
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2025-10-13 20:03:35 -0500

    Time: 4 ms (21.94%), Space: 17.2 MB (35.03%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        unordered_map<string,function<long long(long long, long long)>> map = {
-            {"+",[](long long a, long long b){return a+b;}},
-            {"-",[](long long a, long long b){return a-b;}},
-            {"*",[](long long a, long long b){return a*b;}},
-            {"/",[](long long a, long long b){return a/b;}}
-        };
-        stack<long long> stack;
+        stack<int> stk;
         for(int i = 0; i < tokens.size(); i++){
-            string temp = tokens[i];
-            if(temp != "-" and temp != "+" and temp != "*" and temp != "/"){
-                // int isnegative = 1;
-                // int i = 0;
-                // if(temp[0] == '-'){
-                //     isnegative = -1;
-                //     i++;
-                // }
-                // long long curr = 0;
-                // for(; i < temp.size(); i++){
-                //     curr = curr * 10 + temp[i] - '0';
-                // }
-                // curr = curr * isnegative;
-                // stack.push(curr);
-                stack.push(stoll(temp));
-            }else{
-                int a = stack.top();
-                stack.pop();
-                int b = stack.top();
-                stack.pop();
-                stack.push(map[temp](b,a));
+            if(tokens[i] != "+" and tokens[i] != "-" and tokens[i] != "/" and tokens[i] != "*"){
+                int num = stoi(tokens[i]);
+                stk.push(num);
+            }
+            else{
+                int second = stk.top();
+                stk.pop();
+                int first = stk.top();
+                stk.pop();
+                if(tokens[i] == "+"){
+                    stk.push(first + second);
+                }
+                else if(tokens[i] == "-"){
+                    stk.push(first - second);
+                }
+                else if(tokens[i] == "*"){
+                    stk.push(first * second);
+                }
+                else{
+                    stk.push(first / second);
+                }
             }
         }
-        return (int)stack.top();
+        return stk.top();
     }
 };
