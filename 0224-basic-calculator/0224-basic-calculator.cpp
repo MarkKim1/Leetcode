3c67330c34e8d6886a2300ffd07421ed3eac7aee
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2025-10-13 21:25:22 -0500

    Time: 6 ms (26.85%), Space: 13.9 MB (17.58%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<int> stk;
-        int result = 0;
-        long long curr_number = 0;
-        int sign = 1;
-        for(int i = 0; i < s.size(); i++){
-            if(isdigit(s[i])){
-                curr_number = curr_number * 10 + s[i]-'0';
-            }else if(s[i] == '-'){
-                result += sign * curr_number;
-                curr_number = 0;
-                sign = -1;
-            }else if(s[i] == '+'){
-                result += sign * curr_number;
-                curr_number = 0;
+        long long res = 0;        // current accumulated value
+        int sign = 1;             // sign for the next number (+1 or -1)
+        std::stack<long long> st; // stack holds: prev_res, prev_sign (pair)
+
+        for (size_t i = 0; i < s.size(); ++i) {
+            char c = s[i];
+            if (c == ' ') continue;
+
+            if (isdigit(c)) {
+                long long num = 0;
+                while (i < s.size() && isdigit(s[i])) {
+                    num = num * 10 + (s[i] - '0');
+                    ++i;
+                }
+                --i;                    // step back one because for-loop will ++i
+                res += sign * num;      // apply current sign
+            } else if (c == '+') {
+                sign = 1;               // unary '+' just sets sign
+            } else if (c == '-') {
+                sign = -1;              // unary '-' sets sign to -1
+            } else if (c == '(') {
+                // push context and reset for inner expression
+                st.push(res);
+                st.push(sign);
+                res = 0;
                 sign = 1;
-            }else if(s[i] == '('){
-                stk.push(result);
-                stk.push(sign);
-                result = 0;
-                sign = 1;
-            }else if(s[i] == ')'){
-                result+=sign*curr_number;
-                result*=stk.top();
-                stk.pop();
-                result+=stk.top();
-                stk.pop();
-                curr_number = 0;
-                sign = 1;
+            } else if (c == ')') {
+                int prevSign = (int)st.top(); st.pop();
+                long long prevRes = st.top(); st.pop();
+                res = prevRes + prevSign * res;
             }
         }
-        if(curr_number != 0){
-            result += sign * curr_number;
-        }
-        return result;
+        return (int)res;
     }
 };
