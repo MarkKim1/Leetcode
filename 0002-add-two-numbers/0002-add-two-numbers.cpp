88ffd357c5b03c8f228151eff02a8f2a28036708
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2025-10-14 21:13:33 -0500

    Time: 0 ms (100%), Space: 77.1 MB (45.14%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,22 +11,26 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* result = new ListNode();
-        ListNode* head = result;
-        int a,b,round = 0;
-        while(l1 or l2){
-            result->next = new ListNode();
+        ListNode *result = new ListNode();
+        ListNode *resultHead = result;
+        int carry = 0;
+        while(l1 != NULL || l2 != NULL){
+            int l1val = 0;
+            int l2val = 0;
+            if(l1 != NULL){
+                l1val = l1->val;
+                l1 = l1->next;
+            }
+            if(l2 != NULL){
+                l2val = l2->val;
+                l2 = l2->next;
+            }
+            int toadd = l1val + l2val + carry;
+            result->next = new ListNode(toadd % 10);
+            carry = toadd / 10;
             result = result->next;
-            a = l1 ? l1->val : 0;
-            b = l2 ? l2->val : 0;
-            result->val = (a+b+round) % 10;
-            round = (a+b+round)/10;
-            if(l1)l1 = l1->next;
-            if(l2)l2 = l2->next;
         }
-        if(round == 1){
-            result->next = new ListNode(1);
-        }
-        return head->next;
+        if(carry) result->next = new ListNode(carry);
+        return resultHead ->next;
     }
 };
