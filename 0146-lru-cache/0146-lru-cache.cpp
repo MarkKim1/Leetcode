db890657ab0b92b184d801c135edfdd27bd588da
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2025-10-19 16:11:05 -0500

    Time: 68 ms (75.19%), Space: 173.2 MB (62.52%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,62 +1,32 @@
 class LRUCache {
 public:
-    class node {
-        public:
-        int key;
-        int val;
-        node* next;
-        node* prev;
+    int m_capacity;
+    unordered_map<int,list<pair<int,int>>::iterator> m_map;
+    list<pair<int,int>> m_list;
+    LRUCache(int capacity) :  m_capacity(capacity){
         
-        node(int key, int val){
-            this->key = key;
-            this->val = val;
-            this->next = NULL;
-            this->prev = NULL;
-        }
-    };
-    unordered_map<int,node*>map;
-    int capacity;
-    node* head = new node(0,0);
-    node* tail = new node(0,0);
-    
-    LRUCache(int capacity) {
-        this->capacity = capacity;
-        head->next = tail;
-        tail->prev = head;
-        
-    }
-    void insert(node* currnode){
-        currnode->next = head->next;
-        head->next->prev = currnode;
-        head->next = currnode;
-        currnode->prev = head;
-        map[currnode->key] = currnode;
-    }
-    
-    void remove(node* currnode){
-        map.erase(currnode->key);
-        currnode->next->prev = currnode->prev;
-        currnode->prev->next = currnode->next;
     }
     
     int get(int key) {
-        if(map.find(key) == map.end()){
-            return -1;
-        }
-        node* currnode = map[key];
-        remove(currnode);
-        insert(currnode);
-        return currnode->val;
+        if(m_map.find(key) == m_map.end()) return -1;
+        m_list.splice(m_list.begin(), m_list, m_map[key]);
+        return m_map[key]->second;
     }
     
     void put(int key, int value) {
-        if(map.find(key) != map.end()){
-            remove(map[key]);
+        auto found_iter = m_map.find(key);
+        if(found_iter != m_map.end()){
+            m_list.splice(m_list.begin(),m_list, found_iter->second);
+            m_map[key]->second = value;
+            return;
         }
-        if(map.size() == capacity){
-            remove(tail->prev);
+        if(m_list.size() == m_capacity){
+            int key_to_delete = m_list.back().first;
+            m_list.pop_back();
+            m_map.erase(key_to_delete);
         }
-        insert(new node(key,value));
+        m_list.emplace_front(make_pair(key,value));
+        m_map[key] = m_list.begin();
     }
 };
 
