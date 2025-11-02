735881eb22fff932774d8d70fa6a765775dd24a1
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2025-11-01 22:55:58 -0500

    Time: 29 ms (44.11%), Space: 23.7 MB (25.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int ans = 0;
+        int result = 0;
         for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[i].size(); j++){
+            for(int j = 0; j < grid[0].size(); j++){
                 if(grid[i][j] == '1'){
-                    ans++;
-                    mark(grid,i,j);
+                    bfs(grid, i,j);
+                    result++;
                 }
             }
         }
-        return ans;
+        return result;
     }
-    void mark(vector<vector<char>>& grid, int i, int j){
+    void bfs(vector<vector<char>>& grid, int i, int j){
         queue<pair<int,int>> q;
-        q.push({i,j});
-        int offsets[] = {0,1,0,-1,0};
+        vector<int> offset = {0,1,0,-1,0};
+        q.push(make_pair(i,j));
+
         while(!q.empty()){
             auto curr = q.front();
             q.pop();
-            for(int i = 0; i < 4; i++){
-                int row = curr.first + offsets[i];
-                int col = curr.second + offsets[i + 1];
-                if(min(row,col) >= 0 and row < grid.size() and col < grid[0].size() and grid[row][col] == '1'){
-                    q.push({row,col});
-                    grid[row][col] = '0';
-                }                
+            int row = curr.first;
+            int col = curr.second;
+            grid[row][col] = '0';
+            for(int i = 0; i < offset.size()-1; i++){
+                int newrow = row + offset[i];
+                int newcol = col + offset[i + 1];
+                if(min(newrow,newcol) >= 0 and newrow < grid.size() and newcol < grid[0].size() and grid[newrow][newcol] == '1'){
+                    grid[newrow][newcol] = '0';
+                    q.push(make_pair(newrow, newcol));
+                }
             }
         }
     }
