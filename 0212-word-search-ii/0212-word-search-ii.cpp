d68b17875bfcac4f53fcdbfbff6e4f457a05dc9a
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2025-11-05 23:19:32 -0600

    Time: 259 ms (74.16%), Space: 20.6 MB (21.99%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,55 +1,55 @@
 class TrieNode {
     public:
-    string word = "";
     vector<TrieNode*> children;
-    TrieNode(){
-        children.resize(26);
-        for(int i = 0; i < 26; i++){
-            children[i] = NULL;
+    string word;
+    
+    TrieNode() {
+        children = vector<TrieNode*>(26,nullptr);
+        word = "";
+    }
+    void insert(string word){
+        TrieNode* curr = this;
+        for(int i = 0; i < word.size(); i++){
+            if(curr->children[word[i] - 'a'] == nullptr){
+                curr->children[word[i]-'a'] = new TrieNode();
+            }
+            curr = curr->children[word[i] - 'a'];
         }
+        curr->word = word;
     }
 };
+
 class Solution {
 public:
-    vector<string> ans;
+    vector<string> result;
+    TrieNode* trie;
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        TrieNode* trie = new TrieNode();
-        
-        for(auto word: words){
-            auto temp = trie;
-            for(int i = 0; i < word.size(); i++){
-                if(temp->children[word[i]-'a'] == NULL){
-                    temp->children[word[i]-'a'] = new TrieNode();
-                }
-                temp = temp->children[word[i]-'a'];
-            }
-            temp->word = word;
+        trie = new TrieNode();
+        for(int i = 0; i < words.size(); i++){
+            trie->insert(words[i]);
         }
         for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[0].size(); j++){
-                backtrack(board,trie,i,j);
+            for(int j = 0; j < board[i].size(); j++){
+                dfs(board,trie,i,j);
             }
         }
-        return ans;
+        return result;
     }
-    void backtrack(vector<vector<char>>& board, TrieNode* trie, int i, int j){
-        if(min(i,j) < 0 || i >= board.size() || j >= board[0].size() || board[i][j] == '#'){
-            return;
-        }
-        if(!trie->children[board[i][j]-'a']){
-            return;
-        }
-        trie = trie->children[board[i][j]-'a'];
+
+    void dfs(vector<vector<char>>& board, TrieNode* trie, int i, int j){
+        if(min(i,j) < 0 || i >= board.size() || j >= board[0].size() || board[i][j] == '#') return;
+        char c = board[i][j];
+        if(!trie->children[c-'a']) return;
+        trie = trie->children[c-'a'];
         if(trie->word.size() > 0){
-            ans.push_back(trie->word);
+            result.push_back(trie->word);
             trie->word = "";
         }
-        char c = board[i][j];
         board[i][j] = '#';
-        backtrack(board,trie,i+1,j);
-        backtrack(board,trie,i,j+1);
-        backtrack(board,trie,i-1,j);
-        backtrack(board,trie,i,j-1);
+        dfs(board,trie,i + 1, j);
+        dfs(board,trie,i - 1, j);
+        dfs(board,trie,i, j + 1);
+        dfs(board,trie,i, j - 1);
         board[i][j] = c;
     }
 };
