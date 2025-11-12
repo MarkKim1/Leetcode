d94fc6718a6432342a7e8e3aa40a84ea182063de
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2025-11-12 16:26:29 -0600

    Time: 402 ms (5.07%), Space: 209.1 MB (5.08%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -11,12 +11,12 @@
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        if(!head || !head->next) return head;
-        ListNode* prev = nullptr;
+        if(!head || head->next == NULL) return head;
+        ListNode* prev = NULL;
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(fast and fast->next){
+        while(fast and fast->next != NULL){
             prev = slow;
             slow = slow->next;
             fast = fast->next->next;
@@ -24,31 +24,33 @@ public:
         prev->next = NULL;
         ListNode* l1 = sortList(head);
         ListNode* l2 = sortList(slow);
-        return mergeList(l1,l2);
+
+
+        return merge(l1,l2);
     }
-    ListNode* mergeList(ListNode* l1, ListNode* l2){
-        ListNode* ptr = new ListNode();
-        ListNode* curr = ptr;
+    ListNode* merge(ListNode* l1, ListNode* l2){
+        ListNode* head = new ListNode();
+        ListNode* curr = head;
         while(l1 and l2){
             if(l1->val < l2->val){
-                curr->next = l1;
+                curr->next = new ListNode(l1->val);
                 l1 = l1->next;
             }else{
-                curr->next = l2;
+                curr->next = new ListNode(l2->val);
                 l2 = l2->next;
             }
             curr = curr->next;
         }
         while(l1){
-            curr->next = l1;
+            curr->next = new ListNode(l1->val);
             curr = curr->next;
             l1 = l1->next;
         }
         while(l2){
-            curr->next = l2;
+            curr->next = new ListNode(l2->val);
             curr = curr->next;
             l2 = l2->next;
         }
-        return ptr->next;
+        return head->next;
     }
 };
