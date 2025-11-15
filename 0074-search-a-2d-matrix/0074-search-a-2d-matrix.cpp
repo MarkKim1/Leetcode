5623737c4674ea9e17f35b5e298e7c452edc1fdd
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2025-11-14 22:10:19 -0600

    Time: 0 ms (100%), Space: 13.3 MB (75.79%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int top = 0;
-        int bot = matrix.size()-1;
-        int end = matrix[0].size()-1;
-        while(top <= bot){
-            int mid = (top + bot) / 2;
-            if(target < matrix[mid][0]){
-                bot = mid-1;
-            }else if(target > matrix[mid][end]){
-                top = mid+1;
+        int bottom = matrix.size() - 1;
+        int row;
+        while(top < bottom){
+            int mid = (top + bottom) / 2;
+            if(matrix[mid][matrix[0].size() - 1] == target) return true;
+            if(matrix[mid][matrix[0].size() - 1] < target){
+                top = mid + 1;
             }else{
-                break;
+                bottom = mid;
+            }
+            if(matrix[mid][matrix[0].size() - 1] < target){
+                row = mid + 1;
+            }else{
+                row = mid;
             }
         }
-        if(!(top <= bot)){
-            return false;
-        }
-        int row = (top + bot) / 2;
-        int left = 0,right = matrix[0].size();
+        int left = 0;
+        int right = matrix[0].size() - 1;
         while(left <= right){
-            int mid = (right + left) / 2;
-            if(target < matrix[row][mid]) {
-                right = mid-1;
-            }else if(target > matrix[row][mid]){
+            int mid = (left + right) / 2;
+            if(matrix[row][mid] == target) return true;
+            if(matrix[row][mid] < target){
                 left = mid + 1;
             }else{
-                return true;
+                right = mid - 1;
             }
         }
         return false;
