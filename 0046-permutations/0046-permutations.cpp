992e1c5b6ad871a10cb58f3a7721fd7563ec47de
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2025-12-06 00:15:00 -0600

    Time: 1 ms (42.48%), Space: 11.3 MB (20.34%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    unordered_set<int> set;
     vector<vector<int>> result;
+    unordered_set<int> visited;
+    vector<int> temp;
     vector<vector<int>> permute(vector<int>& nums) {
-        helper(nums,{});
+        helper(nums);
         return result;
     }
-    void helper(vector<int>& nums, vector<int> curr){
-        
+    void helper(vector<int>& nums){
+        if(temp.size() == nums.size()){
+            result.push_back(temp);
+            return;
+        }
         for(int i = 0; i < nums.size(); i++){
-            if(set.find(nums[i]) == set.end()){
-                set.insert(nums[i]);
-                curr.push_back(nums[i]);
-                if(curr.size() == nums.size()){
-                    result.push_back(curr);
-                }
-                helper(nums,curr);
-                set.erase(nums[i]);
-                curr.pop_back();
-            }
+            if(visited.count(nums[i])) continue;
+            temp.push_back(nums[i]);
+            visited.insert(nums[i]);
+            helper(nums);
+            temp.pop_back();
+            visited.erase(nums[i]);
         }
     }
 };
