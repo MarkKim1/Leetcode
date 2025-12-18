e6f19a2646dde780c83ed49eac01471ab893fcb2
Minseok Kim <46219125+MarkKim1@users.noreply.github.com>
2025-12-18 01:27:05 -0600

    Time: 0 ms (100%), Space: 7.9 MB (70.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+    int result = 0;
+        
     int climbStairs(int n) {
-        if(n == 1)return 1;
-        vector<int> dp(n+1);
-        dp[1] = 1;
-        dp[2] = 2;
-        for(int i = 3; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+        vector<int> dp(46,INT_MAX);
+        return helper(n,dp);
+        
+    }
+    int helper(int n,vector<int>& dp){
+        if(n < 0) return 0;
+        if(dp[n] != INT_MAX) return dp[n];
+        if(n == 0) return 1;
+        
+        return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
     }
 };
